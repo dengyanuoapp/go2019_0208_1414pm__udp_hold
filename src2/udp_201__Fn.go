716459f7f0b43133ef0b7799aaadcadef4228e9c
Your Name <you@example.com>
2019-02-15 16:12:16 +0800

    _2019_0215__1612pm

--- src2/udp_201__Fn.go
+++ src2/udp_201__Fn.go
@@ -8,24 +8,28 @@ import (
     //"net"
 )
 
-var (
     //_VuserIpList map[string]string
     //_VuserIpList = map[string]string{}
-    _VserviceCn  _TserviceUDP   
-    _VserviceDn  _TserviceUDP   
-    _VserviceSn  _TserviceUDP   
+
+var (
+    _VserviceUdpCn  _TserviceUDP
+    _VserviceUdpDn  _TserviceUDP
+    _VserviceUdpSn  _TserviceUDP
+
+    _VserviceTcpMo  _TserviceTCP
+
     _Cexit       chan string
     _Clog        chan string
 )
 
 func init() {
-    _VserviceCn.name    = "servicePortForCn" 
-    _VserviceDn.name    = "servicePortForDn" 
-    _VserviceSn.name    = "servicePortForSn" 
+    _VserviceUdpCn.name    = "servicePortForCn"
+    _VserviceUdpDn.name    = "servicePortForDn"
+    _VserviceUdpSn.name    = "servicePortForSn"
 
-    flag.StringVar(&_VserviceCn.port, "c", ":5353",  _VserviceCn.name )
-    flag.StringVar(&_VserviceDn.port, "d", ":32001", _VserviceDn.name )
-    flag.StringVar(&_VserviceSn.port, "s", ":32003", _VserviceSn.name )
+    flag.StringVar(&_VserviceUdpCn.port, "c", ":5353",  _VserviceUdpCn.name )
+    flag.StringVar(&_VserviceUdpDn.port, "d", ":32001", _VserviceUdpDn.name )
+    flag.StringVar(&_VserviceUdpSn.port, "s", ":32003", _VserviceUdpSn.name )
     flag.Parse()
 
     // _FdebugPrintTest()
@@ -39,21 +43,21 @@ func init() {
 
 func main() {
 
-    _FtryListenToUDP01( &_VserviceCn ) 
-    _FtryListenToUDP01( &_VserviceDn ) 
-    _FtryListenToUDP01( &_VserviceSn ) 
+    _FtryListenToUDP01( &_VserviceUdpCn )
+    _FtryListenToUDP01( &_VserviceUdpDn )
+    _FtryListenToUDP01( &_VserviceUdpSn )
 
-    go _FhandleFnClientCn( &_VserviceCn , _Cexit , _Clog )
-    go _FhandleFnClientCn( &_VserviceDn , _Cexit , _Clog )
-    go _FhandleFnClientCn( &_VserviceSn , _Cexit , _Clog )
+    go _FhandleFnWaitForClientCn( &_VserviceUdpCn , _Cexit , _Clog )
+    go _FhandleFnWaitForClientCn( &_VserviceUdpDn , _Cexit , _Clog )
+    go _FhandleFnWaitForClientCn( &_VserviceUdpSn , _Cexit , _Clog )
 
     _Fex( " the reason exit : " + <-_Cexit , nil )
 } // main
 
 // https://golang.org/pkg/net/#UDPConn.ReadFromUDP
 // func (c *UDPConn) ReadFromUDP(b []byte) (int, *UDPAddr, error)
-func _FhandleFnClientCn(___VserviceUDP *_TserviceUDP, ___Cexit chan string , ___Clog chan string ) {
-// (*___VserviceUDP).udpConn 
+func _FhandleFnWaitForClientCn(___VserviceUDP *_TserviceUDP, ___Cexit chan string , ___Clog chan string ) {
+// (*___VserviceUDP).udpConn
     //var __Vbuf [2048]byte             // array : with specified len
     __Vbuf := make( []byte , 2048 )   // silice : with var len
 
@@ -72,4 +76,4 @@ func _FhandleFnClientCn(___VserviceUDP *_TserviceUDP, ___Cexit chan string , ___
     _Fpf( "|%s|" , __Vaddr )
     _PpdN( __Vlen , &__Vbuf )
 
-} // _FhandleFnClientCn
+} // _FhandleFnWaitForClientCn
