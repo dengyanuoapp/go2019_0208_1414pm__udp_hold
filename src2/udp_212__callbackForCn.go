e0f3d9751c9ec97b4d2beda8ef092649933e3b3f
Your Name <you@example.com>
2019-02-16 20:54:33 +0800

    _2019_0216__2054pm

--- src2/udp_212__callbackForCn.go
+++ src2/udp_212__callbackForCn.go
@@ -1,7 +1,6 @@
 package main
 
 func _FcallbackInFnForCn(___VserviceUDP *_TserviceUDP ) {
-    //_Fpf( "|l:%s|r:%s|" , (*___VserviceUDP).hostPortStr , (*___VserviceUDP).VremoteAddr )
-    _Fpf( "|l:%s|r:%s|" , (*___VserviceUDP).VlocalAddr , (*___VserviceUDP).VremoteAddr )
+    _Fpf( "341011 dump |l:%s|r:%s|" , (*___VserviceUDP).VlocalAddr , (*___VserviceUDP).VremoteAddr )
     _PpdN( (*___VserviceUDP).Vlen , &(*___VserviceUDP).Vbuf )
 } // _FcallbackInFnForCn
--- src2/udp_219__callbackForMo.go
+++ src2/udp_219__callbackForMo.go
@@ -3,6 +3,6 @@ package main
 // _TacceptTCP 
 // _TserviceTCP 
 func _FcallbackForDebugMo(___VacceptTcp *_TacceptTCP ) {
-    _Fpf( "|l:%s|r:%s|" , (*___VacceptTcp).VlocalAddr , (*___VacceptTcp).VremoteAddr )
+    _Fpf( "188111 dump|l:%s|r:%s|" , (*___VacceptTcp).VlocalAddr , (*___VacceptTcp).VremoteAddr )
     _PpdN( (*___VacceptTcp).Vlen , &(*___VacceptTcp).Vbuf )
 } // _FcallbackForDebugMo
