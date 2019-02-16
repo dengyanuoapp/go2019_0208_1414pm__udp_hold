b4d1e3c3b1e672290ea7d97be9aece49023a20fd
Your Name <you@example.com>
2019-02-16 22:22:53 +0800

    _2019_0216__2222pm

--- src2/udp_218__callbackForLogService.go
+++ src2/udp_218__callbackForLogService.go
@@ -3,6 +3,7 @@ package main
 // _TacceptTCP 
 // _TserviceTCP 
 func _FcallbackForDebugLog_service(___VacceptTcp *_TserviceTCP ) {
-    _Fpf( "283822 service" )
-    _Pn( )
+    //_Fpf( "283822 service" ); _Pn( )
+    __VprStr := <- (*(*___VacceptTcp).Clog)
+    _FpfN( "283823 service:%s" , __VprStr )
 } // _FcallbackForDebugLog_service
