7d940a615d7ee28fc9207f63062555d18917c742
Your Name <you@example.com>
2019-02-16 20:56:19 +0800

    _2019_0216__2056pm

--- src2/udp_212__callbackForCn.go
+++ src2/udp_212__callbackForCn.go
@@ -1,6 +1,6 @@
 package main
 
 func _FcallbackInFnForCn(___VserviceUDP *_TserviceUDP ) {
-    _Fpf( "341011 dump |l:%s|r:%s|" , (*___VserviceUDP).VlocalAddr , (*___VserviceUDP).VremoteAddr )
+    _Fpf( "341011 udpRdump |l:%s|r:%s|" , (*___VserviceUDP).VlocalAddr , (*___VserviceUDP).VremoteAddr )
     _PpdN( (*___VserviceUDP).Vlen , &(*___VserviceUDP).Vbuf )
 } // _FcallbackInFnForCn
