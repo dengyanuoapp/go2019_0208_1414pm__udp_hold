0cf393a5811f43154f9e49f28ca17025833ebe27
Your Name <you@example.com>
2019-02-16 22:34:37 +0800

    _2019_0216__2234pm

--- src2/udp_219__callbackForLogAccept.go
+++ src2/udp_219__callbackForLogAccept.go
@@ -3,6 +3,11 @@ package main
 // _TacceptTCP 
 // _TserviceTCP 
 func _FcallbackForDebugLog_accept(___VacceptTcp *_TacceptTCP ) {
+
+    /*
     _Fpf( "188111 tcpRdump|l:%s|r:%s|" , (*___VacceptTcp).VlocalAddr , (*___VacceptTcp).VremoteAddr )
     _PpdN( (*___VacceptTcp).Vlen , &(*___VacceptTcp).Vbuf )
+    */
+    (*(*___VacceptTcp).Clog) <- _Pspf( "188112 tcpRdump |l:%s|r:%s|" , (*___VacceptTcp).VlocalAddr , (*___VacceptTcp).VremoteAddr )
+
 } // _FcallbackForDebugLog_accept
