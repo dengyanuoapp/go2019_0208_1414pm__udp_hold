c6dd2a10ea9f366e6e125a6f0bee4e57c9256edc
Your Name <you@example.com>
2019-02-17 21:18:30 +0800

    _2019_0217__2118pm

--- src0/base_101__get_self_md5_sha.go
+++ src0/base_101__get_self_md5_sha.go
@@ -1,12 +1,12 @@
 package main
 
-//import "fmt"
+import "fmt"
 import "os"
 import "io/ioutil"
 import "crypto/md5"
 import "crypto/sha256"
 import "log"
-import "fmt"
+import "encoding/hex"
 
 var (
     _self_md5           [16]byte // md5sum    : 727bf338cf523b90baccd24cca30b919
@@ -24,11 +24,19 @@ func _Fbase_101__get_self_md5_sha() {
     _self_md5 = md5.Sum(__Vcontent)
     _self_sha = sha256.Sum256(__Vcontent)
 
-    log.Printf( " amount : %d\n" , len( os.Args ) )
-    if "" != os.Args[1] && fmt.Sprintf("%x",_self_sha) == os.Args[1] {
-        _debugEnabled = true
+    // prog : 1
+    // prog x1 x2 x3 : 4
+    __VparaLen := len( os.Args )
+    if 1 != __VparaLen {
+        // func hex.DecodeString(s string) ([]byte, error)
+        __Vhex , __Verr := hex.DecodeString( os.Args[ __VparaLen - 1 ] )
+        if nil == __Verr {
+            if fmt.Sprintf("%x" , __Vhex) == fmt.Sprintf("%x" , _self_sha ) {
+                _debugEnabled = true
+            }
+        }
     }
-    log.Printf( " _debugEnabled : %d\n" , _debugEnabled )
+    log.Printf( " _debugEnabled : %t\n" , _debugEnabled )
 
 
     _Ppn("from:", os.Args[0])
