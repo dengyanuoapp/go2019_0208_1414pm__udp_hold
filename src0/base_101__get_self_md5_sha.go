1eb63078fd8171532c31f866c74c60fb7348621f
Your Name <you@example.com>
2019-02-17 20:39:46 +0800

    _2019_0217__2039pm

--- src0/base_101__get_self_md5_sha.go
+++ src0/base_101__get_self_md5_sha.go
@@ -9,8 +9,8 @@ import "log"
 //import "std"
 
 var (
-    _self_md5 []byte // md5sum    : 727bf338cf523b90baccd24cca30b919
-    _self_sha []byte // sha256sum :  2c6e3b458d5c482bc52a1d7d4f5a7d7766381c9f07d9b32ca605ae45b4e473f5
+    _self_md5 [16]byte // md5sum    : 727bf338cf523b90baccd24cca30b919
+    _self_sha [32]byte // sha256sum :  2c6e3b458d5c482bc52a1d7d4f5a7d7766381c9f07d9b32ca605ae45b4e473f5
 )
 
 func _Fbase_101__get_self_md5_sha() {
@@ -20,8 +20,8 @@ func _Fbase_101__get_self_md5_sha() {
         log.Fatalf( "err138191" , __Verr )
     }
 
-    _self_md5 := md5.Sum(__Vcontent)
-    _self_sha := sha256.Sum256(__Vcontent)
+    _self_md5 = md5.Sum(__Vcontent)
+    _self_sha = sha256.Sum256(__Vcontent)
 
     _Ppn("from:", os.Args[0])
     _FpfN("File md5: [ %x ]", _self_md5)
