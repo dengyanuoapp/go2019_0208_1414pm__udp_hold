c63ae558b30304120738a62bcad09482cabc7b1a
Your Name <you@example.com>
2019-02-17 21:31:03 +0800

    _2019_0217__2131pm

--- src0/base_100__printf.go
+++ src0/base_100__printf.go
@@ -40,43 +40,6 @@ func _Fpt        (                  ___Vpara ...interface{}) (int, error)   {
     return _Ppt(           ___Vpara ... )
 } // _Fpt
 
-// dump hex
-func _PpdL( ___Vlen int , ___Vbuf *[]byte) {
-    __Xlen := len( *___Vbuf )
-    if __Xlen > ___Vlen { __Xlen = ___Vlen }
-    _Ppf( "(%d)", __Xlen )
-    __Xlen --
-    for __Vi := 0 ; __Vi <= __Xlen ; __Vi ++ {
-        if __Vi == __Xlen {
-            _Ppf( "%02x", (*___Vbuf)[__Vi] )
-        } else {
-            _Ppf( "%02x ", (*___Vbuf)[__Vi] )
-        }
-    }
-} // _PpdL
-
-//func _FcopyByte( &((*___VacceptTCP).Vbuf2) , &((*___VacceptTCP).Vbuf), (*___VacceptTCP).Vlen )
-//(*___VacceptTCP).Vbuf2 = make([]byte , (*___VacceptTCP).Vlen ); copy( (*___VacceptTCP).Vbuf2 , (*___VacceptTCP).Vbuf )
-func _FcopyByte( ___dst *[]byte , ___src *[]byte , ___len int ) {
-    __len2 := cap( (*___src) ) 
-    if ( ___len > __len2 ) { ___len = __len2 }
-    (*___dst) = make([]byte , ___len )
-    copy( (*___dst) , (*___src) )
-}// _FcopyByte
-
-func _PpdLN( ___Vlen int , ___Vbuf *[]byte) {
-    _PpdL( ___Vlen , ___Vbuf )
-    _Pn()
-} // _PpdLN
-func _Fpd( ___Vlen int , ___Vbuf *[]byte) {
-    _Fph()
-    _PpdL( ___Vlen , ___Vbuf )
-} // _Fpd
-func _FpdN( ___Vlen int , ___Vbuf *[]byte) {
-    _Fpd( ___Vlen , ___Vbuf )
-    _Pn()
-} // _FpdN
-
 //func log.Fatal(v ...interface{})
 //func log.Fatalf(format string, v ...interface{})
 func _Fex( ___Vstr string , ___V interface{} ) {
--- src0/base_101__get_self_md5_sha.go
+++ src0/base_101__get_self_md5_sha.go
@@ -9,8 +9,8 @@ import "log"
 import "encoding/hex"
 
 var (
-    _self_md5           [16]byte // md5sum    : 727bf338cf523b90baccd24cca30b919
-    _self_sha           [32]byte // sha256sum :  2c6e3b458d5c482bc52a1d7d4f5a7d7766381c9f07d9b32ca605ae45b4e473f5
+    _self_md5           []byte // md5sum    : 16 byte : 727bf338cf523b90baccd24cca30b919
+    _self_sha           []byte // sha256sum : 32 byte : 2c6e3b458d5c482bc52a1d7d4f5a7d7766381c9f07d9b32ca605ae45b4e473f5
     _debugEnabled       bool
 )
 
@@ -21,8 +21,8 @@ func _Fbase_101__get_self_md5_sha() {
         log.Fatalf( "err138191" , __Verr )
     }
 
-    _self_md5 = md5.Sum(__Vcontent)
-    _self_sha = sha256.Sum256(__Vcontent)
+    _self_md5 = _FmakeByte( 16 , md5.Sum(__Vcontent) )
+    _self_sha = _FmakeByte( 32 , sha256.Sum256(__Vcontent) )
 
     // prog : 1
     // prog x1 x2 x3 : 4
