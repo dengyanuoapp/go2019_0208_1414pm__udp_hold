15716ce3bcb42883ef64844da511c286cf375aa4
Your Name <you@example.com>
2019-02-17 21:03:52 +0800

    _2019_0217__2103pm

--- src0/base_101__get_self_md5_sha.go
+++ src0/base_101__get_self_md5_sha.go
@@ -6,11 +6,12 @@ import "io/ioutil"
 import "crypto/md5"
 import "crypto/sha256"
 import "log"
-//import "std"
+import "fmt"
 
 var (
-    _self_md5 [16]byte // md5sum    : 727bf338cf523b90baccd24cca30b919
-    _self_sha [32]byte // sha256sum :  2c6e3b458d5c482bc52a1d7d4f5a7d7766381c9f07d9b32ca605ae45b4e473f5
+    _self_md5           [16]byte // md5sum    : 727bf338cf523b90baccd24cca30b919
+    _self_sha           [32]byte // sha256sum :  2c6e3b458d5c482bc52a1d7d4f5a7d7766381c9f07d9b32ca605ae45b4e473f5
+    _debugEnabled       bool
 )
 
 func _Fbase_101__get_self_md5_sha() {
@@ -22,6 +23,13 @@ func _Fbase_101__get_self_md5_sha() {
 
     _self_md5 = md5.Sum(__Vcontent)
     _self_sha = sha256.Sum256(__Vcontent)
+
+    log.Printf( " amount : %d\n" , len( os.Args ) )
+    if "" != os.Args[1] && fmt.Sprintf("%x",_self_sha) == os.Args[1] {
+        _debugEnabled = true
+    }
+    log.Printf( " _debugEnabled : %d\n" , _debugEnabled )
+
 
     _Ppn("from:", os.Args[0])
     _FpfN("File md5: [ %x ]", _self_md5)
