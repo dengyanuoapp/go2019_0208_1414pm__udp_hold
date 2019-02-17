c793dfe08048c3c2fa327eaf7890072d6fe05472
Your Name <you@example.com>
2019-02-17 18:32:42 +0800

    _2019_0217__1832pm

--- src2/udp_218__callbackForLogService.go
+++ src2/udp_218__callbackForLogService.go
@@ -1,10 +1,23 @@
-package main
-
 // _TacceptTCP 
 // _TserviceTCP 
+package main
+
 // note : all debug log begin pushed into Clog will try to redirect to TCP debug monitorS.
-func _FcallbackForDebugLog_service_dataChan(___VacceptTcp *_TserviceTCP ) {
+func _FcallbackForDebugLog_service_dataChan(___VserviceTcp *_TserviceTCP ) {
     //_Fpf( "283822 service" ); _Pn( )
-    __VprStr := <- (*(*___VacceptTcp).Clog)
+    __VprStr := <- (*(*___VserviceTcp).Clog)
     _FpfN( "283823 service:%s" , __VprStr )
+
+    for __Vi:=0 ; __Vi < (*___VserviceTcp).cAmount ; __Vi ++ {
+        if (*___VserviceTcp).acceptTCPs[__Vi].enabled {
+            // the Clog -> CchanMsg
+            (*___VserviceTcp).clientMux.Lock()
+
+            if (*___VserviceTcp).acceptTCPs[__Vi].enabled {
+                (*___VserviceTcp).acceptTCPs[__Vi].CchanMsg <- []byte(__VprStr)
+            }
+
+            (*___VserviceTcp).clientMux.Unlock()
+        }
+    }
 } // _FcallbackForDebugLog_service_dataChan
