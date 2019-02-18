74bd5ccc556f6d68dff8ff161c2ff9328040ab8f
Your Name <you@example.com>
2019-02-18 21:21:27 +0800

    _2019_0218__2121pm

--- src2/udp_222__callbackForFilterDelay01.go
+++ src2/udp_222__callbackForFilterDelay01.go
@@ -12,15 +12,22 @@ func _FcallbackFilterDelay_filter( ___Vf *_TfilterDelay)    {
     //_FpfN( " 818395: filter select start" );
     select {
     case __Vstr := <- ___Vf.  Cswap01 :
-        _FpfN( " 818396: filter swap received " + __Vstr )
-    case __Vbyte := <- (*___Vf.  Cin01)  :
-        //case <- (*___Vf.  Cin01)  :
-        _FpfN( " 818397: filter Cin received " + string(__Vbyte) )
-        ___Vf.  Cout01 <- []byte( " 818398 : filte out \n" )
-        //default:
-        //_Fex( "818399 : what happens ?" , nil)
+        //_FpfN( " 818396: filter swap received " + __Vstr )
+        ___Vf . _Ftry_tran_the_ok_task_to_Dn( __Vstr )
 
+    case __Vbyte := <- (*___Vf.  Cin01)  :
+        _FpfN( " 818397: filter Cin received " + string(__Vbyte) )
+        ___Vf . _Ftry_update_filter_array( __Vbyte )
     }
     _FpfN( " 818399: filter select end" );
 
 } // _FcallbackFilterDelay_filter
+
+func ( ___Vf *_TfilterDelay ) _Ftry_tran_the_ok_task_to_Dn(___Vstr string ) {
+    _FpfN( " 828391: _Ftry_tran_the_ok_task_to_Dn : " )
+    ___Vf.  Cout01 <- []byte( " 828392 : filte out \n" )
+} // _Ftry_tran_the_ok_task_to_Dn
+
+func ( ___Vf *_TfilterDelay ) _Ftry_update_filter_array( ___Vbyte []byte ) {
+    _FpfN( " 838391: update table with Cin received :" + string(___Vbyte) )
+} // _Ftry_update_filter_array
