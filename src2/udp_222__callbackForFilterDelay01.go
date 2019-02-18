fba8a7dbd61904e9d2fcdd4e7f91345198942aaf
Your Name <you@example.com>
2019-02-18 20:12:48 +0800

    _2019_0218__2012pm

--- src2/udp_222__callbackForFilterDelay01.go
+++ src2/udp_222__callbackForFilterDelay01.go
@@ -14,12 +14,13 @@ func _FcallbackFilterDelay_filter( ___Vf *_TfilterDelay)    {
     case __Vstr := <- ___Vf.  Cswap01 :
         _FpfN( " 818396: filter swap received " + __Vstr )
     case __Vbyte := <- (*___Vf.  Cin01)  :
-    //case <- (*___Vf.  Cin01)  :
+        //case <- (*___Vf.  Cin01)  :
         _FpfN( " 818397: filter Cin received " + string(__Vbyte) )
         ___Vf.  Cout01 <- []byte( " 818398 : filte out \n" )
-    default:
-        _Fex( "818399 : what happens ?" , nil)
+        //default:
+        //_Fex( "818399 : what happens ?" , nil)
 
     }
+    _FpfN( " 818399: filter select end" );
 
 } // _FcallbackFilterDelay_filter
