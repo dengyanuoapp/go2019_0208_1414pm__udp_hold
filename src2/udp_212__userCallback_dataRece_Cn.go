752318b360696e5ccc142dabca2190f1431c0cf9
Your Name <you@example.com>
2019-02-19 10:52:41 +0800

    _2019_0219__1052am

--- src2/udp_212__userCallback_dataRece_Cn.go
+++ src2/udp_212__userCallback_dataRece_Cn.go
@@ -7,10 +7,11 @@ import (
 )
 
 type _Tcn2dn struct {
-    version                 int
-    IP                      net.IP
-    Port                    int
-    ipStr                   string
+    version     int
+    idx         int
+    Port        int
+    IP          net.IP
+    ipStr       string
 }
 type _TnodeCn2dn struct {
     cnt             int
@@ -18,21 +19,28 @@ type _TnodeCn2dn struct {
 }
 type _TmapCn2dn     map[string]_TnodeCn2dn
 
+var _VcnIdx int
 func _FuserCallback_dataRece_Cn(___VserviceUDP *_TserviceUDP ) {
 
-    *___VserviceUDP.Clog <- _Pspf( "0738181 Cn receMsg |l:%s|r:%s|(%d)\n" ,
+    _VcnIdx ++
+
+    *___VserviceUDP.Clog <- _Pspf( "0738181 (%d) Cn receMsg |l:%s|r:%s|(%d)\n" ,
+    _VcnIdx,
     ___VserviceUDP.VlocalAddr ,
     ___VserviceUDP.VremoteAddr ,
     ___VserviceUDP.Vlen )
 
+    _Ppf( "0738183 (%d) %s \n" , _VcnIdx , ___VserviceUDP.VremoteAddr.IP.String() )
     if nil != ___VserviceUDP.CuOut01  {
         __Vcn2dn := _Tcn2dn { version : 1, 
         IP      : ___VserviceUDP.VremoteAddr.IP , 
+        idx     : _VcnIdx,
         Port    : ___VserviceUDP.VremoteAddr.Port ,
         ipStr   :  ___VserviceUDP.VremoteAddr.IP.String() }
         // func Marshal(v interface{}) ([]byte, error)
         __Vbyte , __Verr := json.Marshal( __Vcn2dn )
         if nil == __Verr {
+            _Ppt( " 0738185 : Cn pack msg as " ,  string(__Vbyte)+"\n" )
             *___VserviceUDP.CuOut01  <- __Vbyte
         }
     }
