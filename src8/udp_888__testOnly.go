e5abbcb88b425bdf11ec80e20eb623d89d583995
Your Name <you@example.com>
2019-02-19 15:23:42 +0800

    _2019_0219__1523pm

--- src8/udp_888__testOnly.go
+++ src8/udp_888__testOnly.go
@@ -20,11 +20,13 @@ func main() {
         __Vcn2dn.   Idx     = 5
         __Vcn2dn.   Port    = 4301
         __Vcn2dn.   IpStr   = "127.0.0.1"
-        _Ppf( "\n 0738184 : origin msg is: %v\n" ,  __Vcn2dn )
+
+        _Ppf( "\n 1918391 : origin msg is: %v\n" ,  __Vcn2dn )
+
         __Vb2 := _FencGob( __Vcn2dn ) 
-        _Ppf( "\n 0738186 : _FencGob msg is: %d , %x\n" , len(__Vb2) ,  __Vb2 )
+        _Ppf( "\n 1918392 : _FencGob gen msg is: %d , %x\n" , len(__Vb2) ,  __Vb2 )
         var __Vt3 _TcnTdn
         _FdecGob( __Vb2 , &__Vt3)
-        _Ppf( "\n 0738187 : _FdecGob msg is: %v\n" ,  __Vt3 )
+        _Ppf( "\n 1918397 : _FdecGob recovery msg is: %v\n" ,  __Vt3 )
 
 } // main
