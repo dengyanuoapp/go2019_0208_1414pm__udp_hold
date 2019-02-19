5c4280b8f1747851abbe4dc3e187d47fe87cff0e
Your Name <you@example.com>
2019-02-19 17:20:04 +0800

    _2019_0219__1720pm

--- src0/base_999__encode_decode.go
+++ src0/base_999__encode_decode.go
@@ -4,21 +4,21 @@ import (
 	"bytes"
 	"encoding/binary"
 	"encoding/gob"
-	//"log"
 
-	"fmt"
+    "encoding/json"
+
+	//"log"
+	//"fmt"
     //"math"
 )
 
 func _FencBin(___V interface{}) []byte {
-    buf := new(bytes.Buffer)
-	//var pi float64 = math.Pi
-	//err := binary.Write(buf, binary.LittleEndian, pi)
-	err := binary.Write(buf, binary.LittleEndian, ___V)
-	if err != nil {
-		fmt.Println("binary.Write failed:", err)
+    __VbBbuf := new(bytes.Buffer)
+	__Verr := binary.Write(__VbBbuf, binary.LittleEndian, ___V)
+	if __Verr != nil {
+        _Perr( __Verr , "1831913 : binary.Write failed:" )
 	}
-	return buf.Bytes()
+	return __VbBbuf.Bytes()
 } // _FencBin
 
 func _FencGob(___V interface{}) []byte {
@@ -40,3 +40,35 @@ func _FdecGob(___Vbyte []byte, ___Vout interface{}) {
         _Perr( __Verr , "1831917 : gob.NewDecoder failed:" )
 	}
 } // _FdecGob
+
+//  func Value.Indirect(v Value) Value
+func _FencRF(___V interface{}) []byte {
+
+    /*
+    var __Vbyte []byte
+    __Vbyte = *((*[]byte)(&___V))
+
+	size := dataSize(v)
+    */
+
+	//buf := make([]byte, size)
+    return nil
+} // _FencRF
+
+//    func Marshal(v interface{}) ([]byte, error)
+func _FencJson(___V interface{}) ([]byte , error) {
+	__VbByte , __Verr := json.Marshal( ___V )
+	if __Verr != nil {
+        _Perr( __Verr , "1831918 : gob.NewDecoder failed:" )
+        return nil , __Verr
+	}
+	return __VbByte , __Verr
+} // _FencJson
+
+//    func Unmarshal(data []byte, v interface{}) error
+func _FdecJson(___Vbyte []byte, ___Vout interface{}) {
+	__Verr := json.Unmarshal( ___Vbyte , ___Vout )
+	if __Verr != nil {
+        _Perr( __Verr , "1831919 : gob.NewDecoder failed:" )
+	}
+} // _FdecJson
