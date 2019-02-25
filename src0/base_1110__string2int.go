3b570cdb0beb568fa2c3f5c9f50086c0cf7832aa
Your Name <you@example.com>
2019-02-25 14:33:06 +0800

    _2019_0225__1433pm

--- src0/base_1110__string2int.go
+++ src0/base_1110__string2int.go
@@ -9,14 +9,17 @@ var (
 
 // func strconv.ParseUint(s string, base int, bitSize int) (uint64, error) {
 func _Fhexstr2uint64( ___Vstr string ) uint64 {
-        var __Vu64 uint64 
+        var __Vu64 uint64
         var __Verr error
-        __Vu64 , __Verr = strconv.ParseUint( ___Vstr , 16, 0 ) // try to read as hex without 0x prefix
+        __Vu64 , __Verr = strconv.ParseUint( ___Vstr , 16, 64 ) // try to read as hex without 0x prefix
         if ( nil != __Verr ) {
-            __Vu64 , __Verr = strconv.ParseUint( ___Vstr , 0, 0 ) // try to read as hex with 0x prefix
+            //_FpfN("893893891 err : %v" , __Verr )
+            __Vu64 , __Verr = strconv.ParseUint( ___Vstr , 0, 64 ) // try to read as hex with 0x prefix
         }
         if ( nil == __Verr ) {
+            //_FpfN("893893893 ok : %x" , __Vu64 )
             return __Vu64
         }
+        _FpfN("893893895 err : %v" , __Verr )
         return 0
 } // _Fhexstr2uint64
