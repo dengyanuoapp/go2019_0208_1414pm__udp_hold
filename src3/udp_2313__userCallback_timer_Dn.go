0561be93bff2d1eb7257360b91b45bcf49e23dcb
Your Name <you@example.com>
2019-02-25 09:36:35 +0800

    _2019_0225__0936am

--- src3/udp_2313__userCallback_timer_Dn.go
+++ src3/udp_2313__userCallback_timer_Dn.go
@@ -6,6 +6,7 @@ import (
     //"net"
     "time"
     "os"
+    "strconv"
 )
 
 
@@ -21,7 +22,15 @@ func _FuserCallback_u03TM__timer_Dn ( ___Vsvr *_TserviceUDP )  {
     _VuExtTimer_Dn . idx ++
 
     _FpfN( " 839111 : %d : trying to Connect to Fn using key 0x%x " , _VuExtTimer_Dn . idx , _VuExtTimer_Dn . c641 )
-    _FpfN( " 839112 : var pwhex : %x , %s" , os.Getenv("pwhex") , os.Getenv("pwhex") );
+
+    // func strconv.ParseUint(s string, base int, bitSize int) (uint64, error)
+    //__Vu64 , __Verr := strconv.ParseUint( os.Getenv("idhex") , 16, 64 ) // : invalid syntax : when 0x prefix
+    __Vu64 , __Verr := strconv.ParseUint( os.Getenv("idhex") , 0, 0 )
+    if ( __Verr == nil ) {
+        _FpfN( " 839112 : var idhex : %x , %s , %x" , os.Getenv("idhex") , os.Getenv("idhex") , __Vu64 )
+    } else {
+        _FpfN( " 839113 : var idhex : %x , %s , convert to u64 error : %v" , os.Getenv("idhex") , os.Getenv("idhex") , __Verr )
+    }
 
     if ( _VuExtTimer_Dn . enabled ) {
         _Fsleep_1s() ;
