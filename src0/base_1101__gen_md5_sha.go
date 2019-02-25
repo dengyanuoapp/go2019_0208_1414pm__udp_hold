3a258ed5c402081038fba2cd623c45503f9c1e8a
Your Name <you@example.com>
2019-02-25 15:13:25 +0800

    _2019_0225__1513pm

--- src0/base_1101__gen_md5_sha.go
+++ src0/base_1101__gen_md5_sha.go
@@ -10,6 +10,7 @@ import (
     //"encoding/hex"
     //"time"
     "strconv"
+    "encoding/binary"
 )
 
 type _Tb256          struct {
@@ -32,7 +33,7 @@ func _Fbase_1101a__gen_shaOnly( ___VbyteIn []byte ) ([]byte) {
 
 func _Fbase_1101b__gen_md5Only( ___VbyteIn []byte ) ([]byte) {
     __Vb    :=  _FmakeByte16( md5.Sum( ___VbyteIn ) )
-    _FpfN( "289891 _Fbase_1101b__gen_md5Only %d [%x]" , len( ___VbyteIn ) , md5.Sum( ___VbyteIn ) )
+    _FpfN( " 289891 _Fbase_1101b__gen_md5Only %d [%x] (%x)" , len( ___VbyteIn ) , md5.Sum( ___VbyteIn ) , __Vb )
     return __Vb
 } // _Fbase_1101b__gen_md5Only
 
@@ -46,7 +47,14 @@ func ( ___Vsha _Tb256 ) _Fbase_1101__gen_shaT( ___VshaInput []byte ) {
 
 func ( ___Vmd5 _Tb128 ) _Fbase_1101__gen_md5T( ___Vmd5Input []byte ) {
     ___Vmd5 . b128      =   _Fbase_1101b__gen_md5Only( ___Vmd5Input )
-    ___Vmd5 . A1 , _    =   strconv.ParseUint( string(___Vmd5 . b128[0:7])      , 16, 64 )
-    ___Vmd5 . A2 , _    =   strconv.ParseUint( string(___Vmd5 . b128[8:15])     , 16, 64 )
+    //___Vmd5 . A1 , _    =   strconv.ParseUint( string(___Vmd5 . b128[0:7])      , 16, 64 )
+    //___Vmd5 . A2 , _    =   strconv.ParseUint( string(___Vmd5 . b128[8:15])     , 16, 64 )
+    //encoding/binary
+    //for __Vi:=0 ; __Vi < 8   ; __Vi ++ { ___Vmd5 . A1 +=   (uint8) (___Vmd5 . b128[__Vi]) }
+    //for __Vi:=8 ; __Vi < 16  ; __Vi ++ { ___Vmd5 . A1 +=   (uint8) (___Vmd5 . b128[__Vi]) }
+    ___Vmd5 . A1 =   binary.BigEndian.Uint64( ___Vmd5 . b128[0:8])
+    ___Vmd5 . A2 =   binary.BigEndian.Uint64( ___Vmd5 . b128[8:16])
+
+    _FpfN( " 289893 _Fbase_1101b__gen_md5Only [%x] (%x %x)" , ___Vmd5 . b128 , ___Vmd5 . A1 , ___Vmd5 . A2 )
 } // _Fbase_1101__gen_md5T
 
