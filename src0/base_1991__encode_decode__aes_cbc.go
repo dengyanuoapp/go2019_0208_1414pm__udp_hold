6567df856eb1bea4711e705c9ba9beacf202e269
Your Name <you@example.com>
2019-03-01 17:52:53 +0800

    _2019_0301__1752pm

--- src0/base_1991__encode_decode__aes_cbc.go
+++ src0/base_1991__encode_decode__aes_cbc.go
@@ -14,39 +14,49 @@ type _Taes struct {
 
 // https://golang.org/pkg/crypto/cipher/#example_NewCBCEncrypter
 func _FencAesCbc__only___(___Vkey *[]byte, ___Viv *[]byte, ___VbyteIn *[]byte) ([]byte, error) {
-	var __VlenIn, __Vlen2, __Vi, __Volen int
-	var __Vtmp, __Vout []byte
+	var (
+		__VlenIn, __VoLen, __VtLen, __VlenAdd, __Vi int
+		__Vtmp, __Vout                              []byte
+		__VneedPat                                  bool
+	)
 
 	//_FpfN(" 132811 _FencAesCbc__only___ : len In (%d) , key %x , iv %x", len(*___VbyteIn), *___Vkey, *___Viv)
 
 	__VlenIn = len(*___VbyteIn)
+	_FnotEqExit(" 132811 ", 16, __VlenIn)
+	_FnotEqExit(" 132813 ", 16, len(*___Viv))
+
 	__Vi = __VlenIn & 0xF
+	__VneedPat = (0 != __Vi)
+	if __VneedPat {
+		__VlenAdd = 16 - __Vi
+		__VtLen = __VlenIn + __VlenAdd
+		__Vtmp = make([]byte, __VtLen)
+		__Vpat := _FgenRand_nByte__(uint16(__VlenAdd))
+		copy(__Vtmp[__VlenIn:], __Vpat)
+		copy(__Vtmp, *___VbyteIn)
+		__VoLen = __VtLen + 16
 
-	if 0 != __Vi {
-		__Vlen2 = __VlenIn + 16 - __Vi
-		__Vtmp = make([]byte, __Vlen2)
-		copy(__Vtmp, (*___VbyteIn))
-		__Volen = __Vlen2 + 16
-
-		_FpfN(" 132812 : use tmp")
+		_FpfN(" 132815 : add pat %d", __VlenAdd)
 	} else {
-		__Volen = __VlenIn + 16
+		__VoLen = __VlenIn + 16
 	}
 
-	__Vout = make([]byte, __Volen)
-	copy(__Vout, (*___Viv)[0:16])
+	__Vout = make([]byte, __VoLen)
+
+	copy(__Vout[0:16], (*___Viv))
 
 	__Vblock, __Verr := aes.NewCipher(*___Vkey)
 	_FerrExit(" 132813 ", __Verr)
 
 	__Vmode := cipher.NewCBCEncrypter(__Vblock, *___Viv)
-	if 0 == __Vi {
-		__Vmode.CryptBlocks(__Vout[16:], *___VbyteIn)
-	} else {
+	if __VneedPat {
 		__Vmode.CryptBlocks(__Vout[16:], __Vtmp)
+	} else {
+		__Vmode.CryptBlocks(__Vout[16:], (*___VbyteIn))
 	}
 
-	if 2 == 3 {
+	if 3 == 3 {
 		_FpfhexN(&__Vout, 32, " 132819 _FencAesCbc__only___ : lenIn %d , dataOut: ", __VlenIn)
 	}
 
