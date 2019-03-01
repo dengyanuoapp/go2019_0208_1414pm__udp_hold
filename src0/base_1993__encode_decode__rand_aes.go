b87c970dd9c93104824e071ac1fc23d1b1e17559
Your Name <you@example.com>
2019-03-01 14:20:10 +0800

    _2019_0301__1420pm

--- src0/base_1993__encode_decode__rand_aes.go
+++ src0/base_1993__encode_decode__rand_aes.go
@@ -10,33 +10,33 @@ var (
 	_VencAesRand_iv128__last []byte = make([]byte, 16)
 )
 
-func _FencAesRand__gen_iv__by_timeMd5() {
-	_VencAesRand_iv128__now := _FgenMd5_now1___(&_VencAesRand_iv128__last)
-	_VencAesRand_iv128__last = _VencAesRand_iv128__now
-} // _FencAesRand__gen_iv__by_timeMd5
+// func _FencAesRand__gen_iv__by_timeMd5() {
+// 	_VencAesRand_iv128__now := _FgenMd5_now1___(&_VencAesRand_iv128__last)
+// 	_VencAesRand_iv128__last = _VencAesRand_iv128__now
+// } // _FencAesRand__gen_iv__by_timeMd5
 
-func _FencAesRand_only(___Vkey *[]byte, ___VbyteIn *[]byte) ([]byte, error) {
-	var __Vb []byte
-	_FpfN(" 192393 key (%d) %x , byteIn (%d) %x , %s ", len(*___Vkey), *___Vkey, len(*___VbyteIn),
-		*___VbyteIn, string(*___VbyteIn))
+func _FencAesRand__only(___Vkey *[]byte, ___VbyteIn *[]byte) ([]byte, error) {
+	var __Vtmp []byte
 
-	_FencAesRand__gen_iv__by_timeMd5()
-	_FpfN(" 192394 gen new iv (%d) %x , old iv (%d) %x", len(_VencAesRand_iv128__now), _VencAesRand_iv128__now,
-		len(_VencAesRand_iv128__last), _VencAesRand_iv128__last)
+	__Viv := _FgenRand_nByte__(16)
+	__VlenIn := len(*___VbyteIn)
+	_FpfhexN(___VbyteIn, 16, " 192391 key %x , iv %x , byteIn ", *___Vkey, __Viv)
 
-	__Vlen := len(*___VbyteIn)
+	__Vtmp = make([]byte, 2+__VlenIn+16) // 2 byte len , data , 16byteMd5
 
-	__Vb = make([]byte, 2)
-	__Vb[1] = byte(__Vlen & 0xFF)
-	__Vlen >>= 8
-	__Vb[0] = byte(__Vlen & 0xFF)
+	__Vtmp[1] = byte(__VlenIn & 0xFF)
+	__VlenIn >>= 8
+	__Vtmp[0] = byte(__VlenIn & 0xFF)
 
-	__Vb = append(__Vb, (*___VbyteIn)...)
-	__Vb = append(__Vb, _FmakeByte16(md5.Sum(*___VbyteIn))...)
-	//__Vb := _FmakeByte32(
-	_FpfN(" 192395 byte (%d) %x , %s ", len(__Vb), __Vb, string(__Vb))
+	//__Vtmp = append(__Vtmp, (*___VbyteIn)...)
+	//__Vtmp = append(__Vtmp, _FmakeByte16(md5.Sum(*___VbyteIn))...)
+	copy(__Vtmp[2:], *___VbyteIn)
+	copy(__Vtmp[2+__VlenIn:], _FmakeByte16(md5.Sum(*___VbyteIn)))
+	_Fex1("838331 Debug Exit ")
 
-	__Vout, __Verr := _FencAesCbc__only___(___Vkey, &_VencAesRand_iv128__now, &__Vb)
+	_FpfN(" 192395 byte (%d) %x , %s ", len(__Vtmp), __Vtmp, string(__Vtmp))
+
+	__Vout, __Verr := _FencAesCbc__only___(___Vkey, &_VencAesRand_iv128__now, &__Vtmp)
 	_FerrExit(" 192396 ", __Verr)
 
 	if len(__Vout) > 16 {
@@ -50,16 +50,16 @@ func _FencAesRand_only(___Vkey *[]byte, ___VbyteIn *[]byte) ([]byte, error) {
 	_FpfN(" 192398 byte (%d) %x , %s ", len(__Vout), __Vout, string(__Vout))
 
 	return __Vout, __Verr
-} // _FencAesRand_only
+} // _FencAesRand__only
 
 func _FencAesRandExit(___VeMsg string, ___Vkey *[]byte, ___VbyteIn *[]byte) []byte {
-	__Vb, __Verr := _FencAesRand_only(___Vkey, ___VbyteIn)
+	__Vb, __Verr := _FencAesRand__only(___Vkey, ___VbyteIn)
 	_FerrExit(___VeMsg+" 292391 ", __Verr)
 	return __Vb
 } // _FencAesRandExit
 
 func _FdecAesRandExit(___VeMsg string, ___Vkey *[]byte, ___VbyteIn *[]byte) []byte {
-	__Vb, __Verr := _FencAesRand_only(___Vkey, ___VbyteIn)
+	__Vb, __Verr := _FencAesRand__only(___Vkey, ___VbyteIn)
 	_FerrExit(___VeMsg+" 292395 ", __Verr)
 	return __Vb
 } // _FdecAesRandExit
@@ -80,5 +80,5 @@ func _FaesRand_test__en_de_Exit(___VstrIn string) {
 	__VstrO := string(_FaesRand_test__decode(" 182814 ", &__Vkey, &__Vt1))
 
 	_FnotEqExit(" 182818 : comp error.", ___VstrIn, __VstrO)
-	_FpfN(" 182819 : comp ok.")
+	_FpfN(" 182819 : comp ok:%s", ___VstrIn)
 } // _FaesRand_test__en_de_Exit
--- src7/udp_7001__json_generator.go
+++ src7/udp_7001__json_generator.go
@@ -15,18 +15,21 @@ var (
 // _FencAesRandExit
 func _Ftry_gen_json01(___Vfname string, ___Vkey *[]byte, ___Vdst interface{}) {
 
-	// _FreGenRandBuf___
-	// _FgenRand_nByte__
-	_FgenRand_nByte__testExit(1) // 1s
-	//_FgenRand_nByte__testExit(1000)       // 6s
-	//_FgenRand_nByte__testExit(10000)      // 37s
-	//_FgenRand_nByte__testExit(100000)     // 300s
-	//_FgenRand_nByte__testExit(1000000)    // 3000s
-	_Fex1(" 389181 debug stop ")
+	if 2 == 3 {
+		// _FreGenRandBuf___
+		// _FgenRand_nByte__
+		_FgenRand_nByte__testExit(1) // 1s
+		//_FgenRand_nByte__testExit(1000)       // 6s
+		//_FgenRand_nByte__testExit(10000)      // 37s
+		//_FgenRand_nByte__testExit(100000)     // 300s
+		//_FgenRand_nByte__testExit(1000000)    // 3360s
+		_Fex1(" 389181 debug stop ")
+	}
 
 	_FaesRand_test__en_de_Exit("asa90as90sa90as90as9as82391")
+	_Fex1(" 389181 debug stop ")
 
-	_Fwrite_json_and_rand_Exit(" 192181 ", ___Vkey, "json/"+___Vfname+".json", ___Vdst)
+	_Fwrite_json_and_rand_Exit(" 389189 ", ___Vkey, "json/"+___Vfname+".json", ___Vdst)
 } // _Ftry_gen_json01
 
 func main() {
