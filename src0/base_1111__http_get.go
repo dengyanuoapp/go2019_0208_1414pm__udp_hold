c1e94bbc6f86e5f6a099ffafb65dba83a6c389e9
Your Name <you@example.com>
2019-03-02 10:55:42 +0800

    _2019_0302__1055am

---
+++ src0/base_1111__http_get.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func _Fhttp_get(filepath string) ([]byte, error) {
+
+	// https://golang.org/pkg/net/http/#Get
+	// "net/http" : func Get(url string) (resp *Response, err error)
+	__Vresp, __Verr := http.Get(url)
+	if __Verr != nil {
+		return nil, __Verr
+	}
+	defer __Vresp.Body.Close() // make sure close.
+
+	// Check server response
+	if __Vresp.StatusCode != http.StatusOK {
+		_FpfN(" 381911 bad status: %s", __Vresp.Status)
+		return nil, fmt.Errorf("381912 bad status: %s", __Vresp.Status)
+	}
+
+	// https://golang.org/pkg/net/http/#Response
+	// import "io/ioutil" : func ReadAll(r io.Reader) ([]byte, error)
+	__Vout, __Verr := ioutil.ReadAll(__Vresp.Body)
+	if nil != __Verr {
+		_FpfN(" 381914 %v", __Verr)
+		return nil, __Verr
+	}
+	return __Vout, nil
+} // _Fhttp_get
--- src0/base_1997__encode_decode__json.go
+++ src0/base_1997__encode_decode__json.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//"bytes"
+	"bytes"
 	//"encoding/binary"
 	//"encoding/gob"
 
@@ -49,27 +49,44 @@ func _FencJsonExit(___VeMsg string, ___V interface{}) []byte {
 } // _FencJsonExit
 
 //    func Unmarshal(data []byte, v interface{}) error
-func _FdecJson(___Vbyte []byte, ___Vout interface{}) {
-	__Verr := json.Unmarshal(___Vbyte, ___Vout)
+func _FdecJson(___Vbyte *[]byte, ___Vout interface{}) {
+	__Verr := json.Unmarshal(*___Vbyte, ___Vout)
 	if __Verr != nil {
 		_Perr(__Verr, "1831919 : gob.NewDecoder failed:")
 	}
 } // _FdecJson
 
-func _Fwrite_json_only_Exit(___VeMsg string, ___Vfname string, ___Vdst interface{}) {
+func _Fwrite_json_only_Exit(___VeMsg string, ___Vfname string, ___Vobj interface{}) []byte {
 	var __Vb []byte
-	__Vb = _FencJsonExit(___VeMsg+" 381911 ", ___Vdst)
+	__Vb = _FencJsonExit(___VeMsg+" 381911 ", ___Vobj)
 	_FwriteFileExit(___VeMsg+" 381912 ", ___Vfname, &__Vb)
+	return __Vb
 } // _Fwrite_json_only_Exit
 
-func _Fwrite_json_rand_only_Exit(___VeMsg string, ___Vkey *[]byte, ___Vfname string, ___Vdst interface{}) {
-	var __Vb []byte
-	__Vb = _FencJsonExit(___VeMsg+" 381915 ", ___Vdst)
-	__Vb = _FencAesRandExit(___VeMsg+" 381916 ", ___Vkey, &__Vb)
-	_FwriteFileExit(___VeMsg+" 381917 ", ___Vfname, &__Vb)
+func _Fwrite_json_rand_only_Exit(___VeMsg string, ___Vkey *[]byte, ___Vfname string, ___Vobj interface{}) {
+	var __VbufTmp1, __VbufTmp2 []byte
+	__VbufTmp1 = _FencJsonExit(___VeMsg+" 381913 ", ___Vobj)
+	__VbufTmp2 = _FencAesRandExit(___VeMsg+" 381914 ", ___Vkey, &__VbufTmp1)
+	_FwriteFileExit(___VeMsg+" 381915 ", ___Vfname, &__VbufTmp2)
 } // _Fwrite_json_rand_only_Exit
 
-func _Fwrite_json_and_rand_Exit(___VeMsg string, ___Vkey *[]byte, ___Vfname string, ___Vdst interface{}) {
-	_Fwrite_json_only_Exit(___VeMsg+" 381918 ", ___Vfname, ___Vdst)
-	_Fwrite_json_rand_only_Exit(___VeMsg+" 381919 ", ___Vkey, ___Vfname+".rand", ___Vdst)
-} // _Fwrite_json_and_rand_Exit
+func _Fread_json_rand_only_Exit(___VeMsg string, ___Vkey *[]byte, ___Vfname string) []byte {
+	var __VbufTmp1, __VbufTmp2 []byte
+	__VbufTmp1 = _FreadFileExit(___VeMsg+" 819183 ", ___Vfname)
+	__VbufTmp2 = _FdecAesRandExit(___VeMsg+" 819184 ", ___Vkey, &__VbufTmp1)
+	//_FdecJson(___VeMsg+" 819185 ", ___Vobj)
+	_FpfhexN(&__VbufTmp2, 80, " 819186 ")
+	return __VbufTmp2
+} // _Fread_json_rand_only_Exit
+
+func _FtestER__write_json_and_rand_Exit(___VeMsg string, ___Vkey *[]byte, ___Vfname string, ___Vobj interface{}) {
+	__VbufText1 := _Fwrite_json_only_Exit(___VeMsg+" 381916 ", ___Vfname, ___Vobj)
+	_Fwrite_json_rand_only_Exit(___VeMsg+" 381917 ", ___Vkey, ___Vfname+".rand", ___Vobj)
+	__VbufText2 := _Fread_json_rand_only_Exit(___VeMsg+" 381918 ", ___Vkey, ___Vfname+".rand")
+
+	_FfalseExit(" 381919 ", bytes.Equal(__VbufText1, __VbufText2))
+
+	_FpfhexN(&__VbufText1, 80, " 819181 ")
+	_FpfhexN(&__VbufText2, 80, " 819181 ")
+	_FpfN(" 819181 : <%s>", __VbufText2)
+} // _FtestER__write_json_and_rand_Exit
