347f001412ccd66a10f46cb3f91d9a22d1b58455
Your Name <you@example.com>
2019-03-03 16:59:38 +0800

    _2019_0303__1659pm

--- src0/base_1203__get_self_md5_sha.go
+++ src0/base_1203__get_self_md5_sha.go
@@ -34,37 +34,37 @@ func _Fbase_1203__detect_debug_enabled() {
 		// func hex.DecodeString(s string) ([]byte, error)
 		__Vhex, __Verr := hex.DecodeString(os.Args[__VparaLen-1])
 		if nil == __Verr {
-			//if fmt.Sprintf("%x" , __Vhex) == fmt.Sprintf("%x" , _Vself.progSha ) {
-			if bytes.Equal(__Vhex, _Vself.progSha.b256) {
-				_Vself.debugEnabled = true
+			//if fmt.Sprintf("%x" , __Vhex) == fmt.Sprintf("%x" , _VS.progSha ) {
+			if bytes.Equal(__Vhex, _VS.progSha.b256) {
+				_VS.debugEnabled = true
 			}
 		}
 	}
-	log.Printf(" _Vself.debugEnabled : %t\n", _Vself.debugEnabled)
+	log.Printf(" _VS.debugEnabled : %t\n", _VS.debugEnabled)
 } // _Fbase_1203__detect_debug_enabled
 
 func _Fbase_1203__gen_self_md5_sha() {
 
-	_Vself.progPath = os.Args[0]
-	__Vcontent, __Verr := ioutil.ReadFile(_Vself.progPath)
+	_VS.progPath = os.Args[0]
+	__Vcontent, __Verr := ioutil.ReadFile(_VS.progPath)
 	if __Verr != nil {
 		log.Fatalf("err138191", __Verr)
 	}
 
-	_Vself.progSha._Fbase_1101__gen_shaT(&__Vcontent)
-	_Vself.progMd5._Fbase_1101__gen_md5T(&__Vcontent)
-	_FpfN(" 838191 _Fbase_1101b__gen_md5Only return : [%x] (%x %x)", _Vself.progMd5.b128, _Vself.progMd5.A1, _Vself.progMd5.A2)
+	_VS.progSha._Fbase_1101__gen_shaT(&__Vcontent)
+	_VS.progMd5._Fbase_1101__gen_md5T(&__Vcontent)
+	_FpfN(" 838191 _Fbase_1101b__gen_md5Only return : [%x] (%x %x)", _VS.progMd5.b128, _VS.progMd5.A1, _VS.progMd5.A2)
 
-	_Ppn("from:", _Vself.progPath)
-	_FpfN(" 8381191 File md5: [ %x ]", _Vself.progMd5.b128)
-	_FpfN(" 8381192 File sha: [ %x ] %x %x %x %x ", _Vself.progSha.b256, _Vself.progSha.A1, _Vself.progSha.A2, _Vself.progSha.A3, _Vself.progSha.A4)
+	_Ppn("from:", _VS.progPath)
+	_FpfN(" 8381191 File md5: [ %x ]", _VS.progMd5.b128)
+	_FpfN(" 8381192 File sha: [ %x ] %x %x %x %x ", _VS.progSha.b256, _VS.progSha.A1, _VS.progSha.A2, _VS.progSha.A3, _VS.progSha.A4)
 
 } // _Fbase_1203__gen_self_md5_sha
 
 func _Fbase_1203__gen_rand_seed() {
-	_Vself.startTime = time.Now()
-	__Vb := []byte(_Pspf("%x", _Vself.startTime))
-	_Vself.startTimEsha._Fbase_1101__gen_shaT(&__Vb)
+	_VS.startTime = time.Now()
+	__Vb := []byte(_Pspf("%x", _VS.startTime))
+	_VS.startTimEsha._Fbase_1101__gen_shaT(&__Vb)
 } // _Fbase_1203__gen_rand_seed()
 
 func _Fbase_1203__init_self_All() {
