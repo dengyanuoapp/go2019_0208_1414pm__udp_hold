8560fed935e9bba0b01fc6e1157da49db9cd17cf
Your Name <you@example.com>
2019-03-03 17:04:15 +0800

    _2019_0303__1704pm

--- src0/base_1251__connect_to_server_to_handshake.go
+++ src0/base_1251__connect_to_server_to_handshake.go
@@ -24,6 +24,7 @@ func (___VreqNewSession *_TreqNewSession) _Fconnect_to_server_01__req_new_sessio
 	_FpfN(" 311912 04 %v", _VS)
 	//_FpfN(" 311912 05 %v", _Vconfig)
 	_FpfN(" 311912 06 %v", _VC)
+	_FpfN(" 311912 07 %v", ___VreqNewSession)
 
 	_Fex1(" 311912 09 ")
 } // _Fconnect_to_server_01__req_new_sessionID__main_top
