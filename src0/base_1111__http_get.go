2e5cfb2c1653c14ba3ef5a70f43e94ef8492fb21
Your Name <you@example.com>
2019-03-04 23:42:09 +0800

    _2019_0304__2342pm : add http time out

--- src0/base_1111__http_get.go
+++ src0/base_1111__http_get.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 func _Fhttp_getAll(___skipTLS bool, ___Vuri string) ([]byte, error) {
@@ -55,7 +56,10 @@ func _Fhttp_getLimit(___skipTLS bool, ___Vuri string, ___VmaxLen int64) ([]byte,
 
 	if ___skipTLS == true { // skip tls
 		__Vtransport := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
-		__Vclient := &http.Client{Transport: __Vtransport}
+		__Vtimeout := time.Duration(15 * time.Second)
+		__Vclient := &http.Client{
+			Timeout:   __Vtimeout,
+			Transport: __Vtransport}
 		__Vresp, __Verr = __Vclient.Get(___Vuri)
 	} else { // using tls
 		__Vresp, __Verr = http.Get(___Vuri)
