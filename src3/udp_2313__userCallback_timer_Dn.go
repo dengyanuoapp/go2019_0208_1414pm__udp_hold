13cb98d3066ceb2abc2ac987501177bc06bcb6fd
Your Name <you@example.com>
2019-03-09 13:53:44 +0800

    _2019_0309__1353pm

--- src3/udp_2313__userCallback_timer_Dn.go
+++ src3/udp_2313__userCallback_timer_Dn.go
@@ -19,8 +19,8 @@ var (
 
 	_VsrvInfo_Dn        _TsrvInfo
 	_VUreqNewSession_Dn _TUreqNewSession = _TUreqNewSession{
-		updateUri:    "https://raw.githubusercontent.com/jasas78/jsonOnly/master/json/Dn2Fn.json.rand",
-		updatePasswd: &_Vpasswd_udp_Dn_download_config,
+		updateUri:    "https://raw.githubusercontent.com/jasas78/jsonOnly/master/json/FnWaitDn.json.rand",
+		updatePasswd: &_Vpasswd_udp_FnWdn_download_config,
 		srvInfo:      &_VsrvInfo_Dn,
 		//UnewSessionCall04: _FuserCallback_u04__,
 		//UnewSessionCall01: _FuserCallback_u01__reqNewSession_in_Dn_to_fn,
