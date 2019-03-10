caf5d736c688498c027132adf0cd11d65e70bea5
Your Name <you@example.com>
2019-03-10 10:43:59 +0800

    _2019_0310__1043am

--- src2/udp_2214__userCallback_dataRece_FD.go
+++ src2/udp_2214__userCallback_dataRece_FD.go
@@ -25,35 +25,13 @@ func _FuserCallback_u01M__dataRece_Fn__main_top(___VserviceUDP *_TserviceUDP) {
 		//		if nil == __Verr2 {
 		//			*___VserviceUDP.CuOut01 <- __Vbyte2
 		//		}
-		___VserviceUDP._FuserCallback_u01M__dataRece_Fn__004()
-	} else {
 		_FpfN(" 1738181 02 : %s : no CuOut01  ", ___VserviceUDP.name)
-		___VserviceUDP._FuserCallback_u01M__dataRece_Fn__003()
+	} else {
+		_FpfN(" 1738181 03 : %s : no CuOut01  ", ___VserviceUDP.name)
+		___VserviceUDP._FuserCallback__client_req_accept_FnWaitDn()
 	}
 
 } // _FuserCallback_u01M__dataRece_Fn__main_top _TserviceUDP
-
-func (___VserviceUDP *_TserviceUDP) _FuserCallback_u01M__dataRece_Fn__004() {
-} // _FuserCallback_u01M__dataRece_Fn__004 _TserviceUDP
-
-func (___VserviceUDP *_TserviceUDP) _FuserCallback_u01M__dataRece_Fn__003() {
-	//_FpfNhex(&___VserviceUDP.Vubuf, 40, " 1738182 03 :")
-	//_FpfNhex(&___VserviceUDP.VuSrvInfo.K256, 80, " 1738182 04 : %s :", ___VserviceUDP.name)
-
-	//__Vb3 := _FdecAesRandExit(" 1738182 05 ", &___VserviceUDP.VuSrvInfo.K256, &___VserviceUDP.Vubuf)
-	__Vb3, __Verr3 := _FdecAesRand__only(&___VserviceUDP.VuSrvInfo.K256, &___VserviceUDP.Vubuf)
-	if nil != __Verr3 {
-		_FpfN(" 1738182 06 received error package , ignore it: %v :", __Verr3)
-		return
-	}
-
-	//_FpfNhex(&__Vb3, 80, " 1738182 07 : %s :", __Vb3)
-	_FpfN(" 1738182 08 : %d / %d : %s \n", ___VserviceUDP.Vulen, len(__Vb3), __Vb3)
-
-	if nil != ___VserviceUDP.CuOut01 {
-	}
-
-} // _FuserCallback_u01M__dataRece_Fn__003 _TserviceUDP
 
 var (
 	_VdnReceCnt int
