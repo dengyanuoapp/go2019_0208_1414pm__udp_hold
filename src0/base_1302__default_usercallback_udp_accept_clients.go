51041ad14aed388ce4132ccf5c81ca87488b289f
Your Name <you@example.com>
2019-03-11 23:53:59 +0800

    _2019_0311__2353pm

--- src0/base_1302__default_usercallback_udp_accept_clients.go
+++ src0/base_1302__default_usercallback_udp_accept_clients.go
@@ -1,5 +1,5 @@
 package main
 
 func (___Vacc *_TuAcceptClientMap) _FuserCallback__acceptClient01__default(___Vreq _TreqIneedToLogin, ___Vid [16]byte) {
-	_FpfN(" 668191 01 ")
+	_FpfN(" 668191 01 : accepting %x :%v", ___Vid, ___Vreq)
 } // _FuserCallback__acceptClient01__default
