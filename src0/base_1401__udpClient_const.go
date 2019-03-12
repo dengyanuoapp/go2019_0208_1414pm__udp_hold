aef978cf0114c42cae46a56c7899baf2b1027a2b
Your Name <you@example.com>
2019-03-12 16:01:48 +0800

    _2019_0312__1601pm

---
+++ src0/base_1401__udpClient_const.go
@@ -0,0 +1,17 @@
+package main
+
+// _TserviceUDP
+type _TudpClientSt  struct {
+    ucPw128          [16]byte
+
+	ucAddr          *net.UDPAddr
+	ucConn          *net.UDPConn
+	ucErr           error
+
+	VucBuf          []byte
+	VucLen          int
+	VucRemoteAddr   *net.UDPAddr
+	VucLocalAddr    net.Addr
+	VucSrvInfo      *_TsrvInfo
+}
+
