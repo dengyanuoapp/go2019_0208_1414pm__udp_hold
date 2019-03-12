1be721d77d8d4a41d1d158df314913b2cc32fb28
Your Name <you@example.com>
2019-03-12 19:17:57 +0800

    _2019_0312__1917pm

--- src0/base_1402__udpGroup_default_main.go
+++ src0/base_1402__udpGroup_default_main.go
@@ -4,6 +4,7 @@ package main
 func (___Vun *_TudpNodeSt) IRun(___Vidx int) {
 	switch ___Vidx {
 	case 140201:
+		_FsrvGroup__140201__main_top__default(___Vun)
 	default:
 		_FpfNex(" 937191 09 : unknown IRun : %d ", ___Vidx)
 	} // switch ___Vidx
@@ -13,4 +14,5 @@ func _FsrvGroup__140201__main_top__default(___Vun *_TudpNodeSt) {
 	if nil != ___Vun.unPwIn256LP {
 		copy(___Vun.unPwIn256[:], *___Vun.unPwIn256LP)
 	}
+	_FpfN(" 918381 01 : %x ", ___Vun.unPwIn256)
 }
