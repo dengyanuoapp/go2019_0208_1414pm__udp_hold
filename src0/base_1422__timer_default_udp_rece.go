640cd4e25cbe3a2c58fbaf01f08abbb0ff80dc71
Your Name <you@example.com>
2019-03-14 20:48:48 +0800

    _2019_0314__2048pm

--- src0/base_1422__timer_default_udp_rece.go
+++ src0/base_1422__timer_default_udp_rece.go
@@ -1,5 +1,12 @@
 package main
 
 func _FudpTimer__150201x__filter_receive__default(___Vutm *_TudpTimer) {
+	__VchRece := ___Vutm.uTmCHudpRece.unCHrece
 	_FpfN(" 388193 01 : filte received 150201 ")
+	for {
+		select {
+		case __Vrece := <-(*__VchRece):
+			_FpfN(" 388193 02 : filte received 150201 %T : %+v", __Vrece, __Vrece)
+		}
+	}
 }
