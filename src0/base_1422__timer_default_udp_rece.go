11a81ad43a07b02cf16f6c85c38728e243758648
Your Name <you@example.com>
2019-03-14 15:19:43 +0800

    _2019_0314__1519pm

--- src0/base_1422__timer_default_udp_rece.go
+++ src0/base_1422__timer_default_udp_rece.go
@@ -1,1 +1,5 @@
 package main
+
+func _FudpTimer__150201x__filter_receive__default(___Vutm *_TudpTimer) {
+	_FpfN(" 388193 01 : filte received 150201 ")
+}
--- src0/base_1424__timer_default_irun.go
+++ src0/base_1424__timer_default_irun.go
@@ -14,4 +14,17 @@ func (___Vutm *_TudpTimer) IRun(___Vidx int) {
 
 func _FudpTimer__150001x__init__default(___Vutm *_TudpTimer) {
 	_FpfN(" 838918 01 : udpTimer 150001 ")
+
+	if nil == ___Vutm.uTmCHudpRece {
+		_Fex1(" 838918 02 : you must set the Receive _TudpNodeSt ")
+	}
+
+	__VuRece := make(chan _TudpNodeDataRece, 10)
+	___Vutm.uTmCHudpRece.unCHrece = &__VuRece
+
+	if nil == ___Vutm.uTmCB150201filter {
+		_FudpTimer__150201x__filter_receive__default(___Vutm)
+	} else {
+		___Vutm.uTmCB150201filter(___Vutm)
+	}
 }
