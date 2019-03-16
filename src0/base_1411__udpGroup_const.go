e9b5dd6afe307587904a89d93de8482519d1080d
Your Name <you@example.com>
2019-03-16 14:48:58 +0800

    _2019_0316__1448pm

--- src0/base_1411__udpGroup_const.go
+++ src0/base_1411__udpGroup_const.go
@@ -4,9 +4,9 @@ type _TudpGroupSt struct {
 	ugName        string
 	ugHostPortStr string
 	ugAmount      int
-	ugRKeyLP      *[]byte
-	ugRkey        []_Tkey256
-	ugNarr        []_TudpNodeSt
+	ugRkeyLP      *[]byte
+	ugRkeyS       []*_Tkey256
+	ugNodeS       []_TudpNodeSt
 	ugCHrece      *chan _TudpNodeDataRece // if nil , drop it ; not-nil , put the received data into this chan
 	ugCHsend      chan _TudpNodeDataSend  // try get data from chan, then send it out.
 	ugCBinit      func(*_TudpGroupSt)     //
--- src0/base_1412__udpGroup_default_main.go
+++ src0/base_1412__udpGroup_default_main.go
@@ -15,5 +15,18 @@ func (___Vug *_TudpGroupSt) IRun(___Vidx int) {
 
 // _TudpGroupSt
 func _FudpGroup__150201__main_init__default(___Vug *_TudpGroupSt) {
+	___Vug.ugRkeyS = make([]*_Tkey256, ___Vug.ugAmount)
+	___Vug.ugNodeS = make([]_TudpNodeSt, ___Vug.ugAmount)
+	if nil == ___Vug.ugRKeyLP {
+		for __Vi := 0; __Vi < ___Vug.ugAmount; __vi++ {
+			___Vug.ugNodeS[__Vi].unName = ___Vug.ugName
+			___Vug.ugNodeS[__Vi].unHostPortStr = ___Vug.ugHostPortStr
+			//___Vug.ugRkeyS[__Vi]    = *___Vug.ugRKeyLP
+		}
+	} else {
+		for __Vi := 0; __Vi < ___Vug.ugAmount; __vi++ {
+		}
+	}
+
 	_FpfNex(" 834811 99 : exit.[%#v]", ___Vug)
 }
