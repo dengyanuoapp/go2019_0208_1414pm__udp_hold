5049e111e74860408c8458221f3b07dd79409a04
Your Name <you@example.com>
2019-03-17 09:31:36 +0800

    _2019_0317__0931am

--- src0/base_1425__timer_default_tryUdpConnect.go
+++ src0/base_1425__timer_default_tryUdpConnect.go
@@ -4,6 +4,10 @@ package main
 // _TsrvInfo
 // _TgapTimer
 func _FudpTimer__150102x__init__tryUdpConn__default(___Vgtm *_TgapTimer) {
+	__Vsrv := ___Vgtm.uTmSrvInfo02
 	_Fdebug1(" 138181 01 %#v ", ___Vgtm.uTmSrvInfo02)
 
+	if false == __Vsrv.runing {
+		//__Vsrv
+	}
 }
