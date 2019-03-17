7829976ef74be6d18309d79894a80af501f5bf8e
Your Name <you@example.com>
2019-03-17 21:15:59 +0800

    _2019_0317__2115pm

--- src0/base_1425__timer_default_tryUdpConnect.go
+++ src0/base_1425__timer_default_tryUdpConnect.go
@@ -3,8 +3,9 @@ package main
 // _TreqIneedToLogin
 // _TsrvInfo
 // _TgapTimer
+//uTmGapNewSession2 *_TgapNewSession
 func _FudpTimer__750102x__init__tryUdpConn__default(___Vgtm *_TgapTimer) {
-	__Vns := ___Vgtm.uTmGapNewSession2
+	__VnewSession := ___Vgtm.uTmGapNewSession2
 	_Fdebug1(" 138181 01 %#v ", ___Vgtm.uTmGapNewSession2)
 
 	__Vgap := ___Vgtm.uTmGapX
@@ -15,26 +16,34 @@ func _FudpTimer__750102x__init__tryUdpConn__default(___Vgtm *_TgapTimer) {
 	}
 
 	for {
-		if true == __Vns.connected {
-			__Vns.tryCnt = 0
-			__Vns.skipCnt = 3 // if within 3 * gap , not try re-connect
-			_Fsleep(__Vgap)
-		} else {
-			if 0 == __Vns.tryCnt {
-				if 0 == __Vns.skipCnt {
+		if true == __VnewSession.connected {
+			__VnewSession.tryCnt = 0
+			__VnewSession.skipCnt = 6 // set timeout to 60s , not try re-connect
+		} else { // true != __VnewSession.connected
+			if 0 == __VnewSession.tryCnt {
+				if 0 == __VnewSession.skipCnt {
+					__VsrvInfo := __VnewSession._FudpTimer__750102y__tryGetSrvInfoFromUri()
+					if nil == __VsrvInfo {
+						// do nothing , except set timeoutto nex 30s
+						__VnewSession.skipCnt = 3
+					} else {
+					}
 				} else {
 				}
-			} else {
+			} else { // 0 != __VnewSession.tryCnt
+				if 0 == __VnewSession.skipCnt {
+				} else {
+				}
 			}
 		}
-		//		__Vsrv3 := __Vns._Fanalyze_srvInfo__whether_trying_to_connect()
+		//		__Vsrv3 := __VnewSession._Fanalyze_srvInfo__whether_trying_to_connect()
 		//		if nil != __Vsrv3 {
 		//			//___Vgtm.uTmGapNewSession2
 		//		}
-		_Fsleep(__Vgap)
+		_Fsleep(__Vgap) // mini Gap , at least
 	}
 }
 
-func (___Vsrv *_TsrvInfo) _Fanalyze_srvInfo__whether_trying_to_connect() *_TsrvInfo {
-	return nil
+func (___Vsrv *_TsrvInfo) _FudpTimer__750102y__tryGetSrvInfoFromUri() *_TsrvInfo {
+	return true
 }
