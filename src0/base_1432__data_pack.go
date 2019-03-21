028311f7b78ac6722e4ac5061c0643b530be8dff
Your Name <you@example.com>
2019-03-21 18:10:40 +0800

    _2019_0321__1810pm

--- src0/base_1432__data_pack.go
+++ src0/base_1432__data_pack.go
@@ -1,6 +1,22 @@
 package main
 
+type _TdataPack_991 struct {
+	V [4]byte // version
+	C byte    // cmd
+	D []byte  // gob.package
+}
+
+// _TreqIneedToLogin
 func (___VuConnPort *_TudpConnPort) _FdataPack__101__udpConnPort() *[]byte {
+	__Vreq := _TreqIneedToLogin{
+		MeTime:   _FtimeI64(),
+		ReqStr:   "try_to_login01",
+		MeName:   _VC.Name,
+		MeIdx128: _VC.MyId128,
+		MeSeq128: _VS.meSeq128,
+		//ToIdx128 []byte,
+		//ToSeq128 []byte,
+	}
 	var __VbOut []byte
 	return &__VbOut
 }
