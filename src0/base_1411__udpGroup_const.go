d96c22a7eab31ad4454577ffb3e71edf740459c8
Your Name <you@example.com>
2019-03-21 08:17:49 +0800

    _2019_0321__0817am

--- src0/base_1411__udpGroup_const.go
+++ src0/base_1411__udpGroup_const.go
@@ -6,11 +6,11 @@ type _TudpGroupSt struct {
 	ugAmount       int
 	ugRkeyLP       [](*[]byte)
 	ugNodeS        []_TudpNodeSt
-	ugCHreceLX     *chan _TudpNodeDataRece // if nil , drop it ; not-nil , put the received data into this chan
-	ugCHsendX      chan _TudpNodeDataSend  // try get data from chan, then send it out.
 	ugCHuConnPortX chan _TudpConnPort
 	ugCBinit       func(*_TudpGroupSt) //
 	ugCBrece       func(*_TudpGroupSt) // if nil , use the default procedure to deal with receive
 	ugCBsend       func(*_TudpGroupSt) // if nil , use the default procedure to deal with send
 	ugCBportConn   func(*_TudpGroupSt) // if nil , use the default procedure to deal with send
+	//ugCHreceLX     *chan _TudpNodeDataRece // if nil , drop it ; not-nil , put the received data into this chan
+	//ugCHsendX      chan _TudpNodeDataSend  // try get data from chan, then send it out.
 }
