6d70389baff93ba624f13f575a742b929ce9bb72
Your Name <you@example.com>
2019-03-22 09:44:29 +0800

    _2019_0322__0944am

--- src0/base_1404__udpNode_default_send.go
+++ src0/base_1404__udpNode_default_send.go
@@ -6,15 +6,15 @@ func (___Vun *_TudpNodeSt) _FudpNode__540201z__send() {
 		case __VchSend := <-___Vun.unCHsendX: // _TudpNodeDataSend // usToAddr _TudpConnPort
 			//_FpfN(" 839118 01 send ")
 
-			___Vun._FudpNode__540201zz__try_Rand_buf_before_send()
-			___Vun._FudpNode__540201zzz__send_buf_real()
+			__VchSend._FudpNode__540201zz__try_Rand_buf_before_send()
+			___Vun._FudpNode__540201zzz__send_buf_real(&__VchSend)
 		}
 		//_Fsleep_1s()
 	}
 }
 
-func (___Vun *_TudpNodeSt) _FudpNode__540201zz__try_Rand_buf_before_send() {
-	__Vlen := len(__VchSend.usToAddr.K256)
+func (___Vus *_TudpNodeDataSend) _FudpNode__540201zz__try_Rand_buf_before_send() {
+	__Vlen := len(___Vus.usToAddr.K256)
 	if 0 == __Vlen {
 		return
 	}
@@ -25,16 +25,16 @@ func (___Vun *_TudpNodeSt) _FudpNode__540201zz__try_Rand_buf_before_send() {
 
 }
 
-func (___Vun *_TudpNodeSt) _FudpNode__540201zzz__send_buf_real() {
+func (___Vun *_TudpNodeSt) _FudpNode__540201zzz__send_buf_real(___Vus *_TudpNodeDataSend) {
 	//usToAddr _TudpConnPort
 	// func (c *UDPConn) WriteToUDP(b []byte, addr *UDPAddr) (int, error)
-	_, __Verr2 := ___Vun.unConn.WriteToUDP(__VchSend.usOutBuf, &__VchSend.usToAddr.DstAddr)
+	_, __Verr2 := ___Vun.unConn.WriteToUDP(___Vus.usOutBuf, &___Vus.usToAddr.DstAddr)
 	if __Verr2 != nil {
-		_FpfN(" 839119 06 : udp send error <%s>[%v]", __VchSend.usToAddr.DstAddr, __Verr2)
+		_FpfN(" 839119 06 : udp send error <%s>[%v]", ___Vus.usToAddr.DstAddr, __Verr2)
 		return
 	}
 	if 2 == 2 {
-		_Fpf(" 839119 07 : %s : udp send succeed (%d) ", ___Vun.unName, len(__VchSend.usOutBuf))
-		_Ppf("01 dst<%v>, local<%v>, listen<%v>\n", __VchSend.usToAddr.DstAddr, ___Vun.unLocalAddr, ___Vun.unAddr)
+		_Fpf(" 839119 07 : %s : udp send succeed (%d) ", ___Vun.unName, len(___Vus.usOutBuf))
+		_Ppf("01 dst<%v>, local<%v>, listen<%v>\n", ___Vus.usToAddr.DstAddr, ___Vun.unLocalAddr, ___Vun.unAddr)
 	}
 }
