d83b9dbeb2466a0c2650d2786ebc313eb8e061d0
Your Name <you@example.com>
2019-03-25 09:27:42 +0800

    _2019_0325__0927am

--- src0/base_1422__timer_default_udp_rece.go
+++ src0/base_1422__timer_default_udp_rece.go
@@ -2,11 +2,20 @@ package main
 
 func _FudpTimer__750201x__gap_receive__default(___Vutm *_TgapTimer) {
 	//_FpfNdb(" 388193 01 : filte received start ")
+	if nil != ___Vutm.uTmCHudpReceLX.unCHreceLX {
+		go _FudpTimer__750201x10__gap_receive__default(___Vutm)
+	} else {
+		_FpfNdb(" 388193 09 : why no input channel ?")
+	}
+}
+
+func _FudpTimer__750201x10__gap_receive__default(___Vutm *_TgapTimer) {
+	//_FpfNdb(" 388195 01 : filte received start ")
 	for {
 		//_Fsleep_100s()
 		select {
 		case __Vrece := <-(*___Vutm.uTmCHudpReceLX.unCHreceLX): // _TudpNodeDataRece
-			_FpfNhex(&__Vrece.urrBuf, 38, " 388193 06 : %T , decode len <%d>: %11d", __Vrece.urrBuf, __Vrece.urrLen, _FtimeI64())
+			_FpfNhex(&__Vrece.urrBuf, 38, " 388195 06 : %T , decode len <%d>: %11d", __Vrece.urrBuf, __Vrece.urrLen, _FtimeI64())
 		}
 	}
 }
