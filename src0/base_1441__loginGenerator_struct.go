8a96763f40e4794d34fa15f469450f919616bae6
Your Name <you@example.com>
2019-03-28 17:19:14 +0800

    _2019_0328__1719pm

--- src0/base_1441__loginGenerator_struct.go
+++ src0/base_1441__loginGenerator_struct.go
@@ -1,1 +1,11 @@
 package main
+
+type _TloginGenerator struct {
+	uTmSrvDownInfoLX   *_TsrvDownInfo      // server Info seting , update by Github , try to connect
+	uTmCHugConnPortLO  *chan _TudpConnPort // every gap , gen tryConn package and push into this chan , usually a udpGroupNodeArr
+	uTmCHconnectSuccI  chan bool           // connect/login succeed true/false
+	uTmDecodeOutLX     *chan _Tdecode
+	uTmCB850101init    func(*_TgapTimer) // if nil , use the default init procedure
+	uTmCB850201chRece  func(*_TgapTimer) // if nil , use the default receive
+	uTmCB850301ConnGen func(*_TgapTimer) // if nil , use the default receive
+}
