47ff71640949e870dfc4bc1077e739aac31433d1
Your Name <you@example.com>
2019-03-28 23:58:36 +0800

    _2019_0328__2358pm

--- src2/udp_2201__Fn.go
+++ src2/udp_2201__Fn.go
@@ -6,9 +6,6 @@ import (
 
 var (
 	_VserviceUdp_FnWaitCn _TserviceUDP
-	//_VserviceUdp_FnWaitDn _TserviceUDP
-
-	//_VfilterFn2dn _TfilterDelay
 
 	_VudpNode_FunWaitDun _TudpNodeSt
 
@@ -58,24 +55,12 @@ func _Finit_2201() {
 		Cexit:               &_Cexit,
 		Clog:                &_Clog,
 	}
-	//	_VserviceUdp_FnWaitDn = _TserviceUDP{
-	//		name:             "FnServicePortForDn",
-	//		UuserLoopCall100211: _FuserCallback__20211_dataRece__main_top__FnWaitDn,
-	//		UuserLoopCall100221: _FuserCallback__20221_chanIn__main_top__FnWaitDn,
-	//
-	//		VuSrvInfo: &_TsrvInfo{K256: _Vpasswd_udp_Fn_waitForCliens01},
-	//
-	//		Cexit: &_Cexit,
-	//		Clog:  &_Clog,
-	//	}
 
 	_VudpTimer01 = _TgapTimer{
 		uTmCHunDataReceLI: &_VudpNode_FunWaitDun,
 	}
 
 	flag.StringVar(&_VserviceUdp_FnWaitCn.hostPortStr, "FnWcn", ":53535", _VserviceUdp_FnWaitCn.name)
-	//flag.StringVar(&_VserviceUdp_FnWaitDn.hostPortStr, "FnWdn", ":32001", _VserviceUdp_FnWaitDn.name)
-	//flag.StringVar(&_VudpNode_FunWaitDun.hostPortStr, "FunWdun", ":42001", _VudpNode_FunWaitDun.name)
 	flag.StringVar(&_VudpNode_FunWaitDun.unHostPortStr, "FunWdun", ":32001", _VudpNode_FunWaitDun.unName)
 
 	flag.Parse()
@@ -100,27 +85,12 @@ func main() {
 	//go _Frun(&_VserviceUdp_FnWaitDn, 100201)
 	// ------------------- udp for worker clinet : Cn , Dn , Sn --------- end
 
-	// ------------------- filter between workers --------- begin
-	//	_VfilterFn2dn = _TfilterDelay{
-	//		name:                               "filter_FnReceFromCnDelayNoteDn",
-	//		sleepGap:                           1,
-	//		udpIn:                              &_VserviceUdp_FnWaitCn,
-	//		udpOut:                             &_VserviceUdp_FnWaitDn,
-	//		Fusercallback__320511_filterTheChanIn: _FuserCallback__511_filterDelay_chan_from_FnWaitCn_to_FnWaitDn,
-	//	}
-
-	// IRun _FudpNode__540201__main_init__default
 	// _FudpNode__540211z__receiveCallBack_withTimeGap
 	// _FdataPack__301__dataDecode_loginS1ReqTryNoToken
-	go _Frun(&_VudpNode_FunWaitDun, 540201)
+	go _Frun(&_VudpNode_FunWaitDun, 540201) // IRun _FudpNode__540201__main_init__default
 
-	// IRun _FudpTimer__750101x__init__default
 	// _FudpTimer__750201x__gap_receive__default
-	go _Frun(&_VudpTimer01, 750101)
-
-	//go _VfilterFn2dn. _FfilterDelay501__main_top__default
-	//go _Frun(&_VfilterFn2dn, 501) // IRun
-	// ------------------- filter between workers --------- end
+	go _Frun(&_VudpTimer01, 750101) // IRun _FudpTimer__750101x__init__default
 
 	_Fex(" 893189 99 : the reason exit : "+<-_Cexit, nil)
 } // main
