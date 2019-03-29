739b0058918ac1e4c878243d0d01f858b674ad8f
Your Name <you@example.com>
2019-03-29 22:18:31 +0800

    _2019_0329__2218pm

--- src0/base_1453__loginCheck_step01.go
+++ src0/base_1453__loginCheck_step01.go
@@ -1,5 +1,5 @@
 package main
 
-func _FloginCheck_step102__sReply_token2(___Vdecode *_Tdecode, ____VunCHsend *chan _TudpNodeDataSend) {
+func (___Vlc *_TloginCheck) _FloginCheck_step102__sReply_tokenB(___Vdecode *_Tdecode) {
 	_FpfNdb(" 838393 01 : ...... ")
 }
