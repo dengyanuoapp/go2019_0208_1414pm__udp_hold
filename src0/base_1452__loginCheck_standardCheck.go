ba0bc6663f624d37dda218c5ac46c57c0a818d0f
Your Name <you@example.com>
2019-03-29 21:52:18 +0800

    _2019_0329__2152pm

--- src0/base_1452__loginCheck_standardCheck.go
+++ src0/base_1452__loginCheck_standardCheck.go
@@ -18,6 +18,7 @@ func (___Vlc *_TloginCheck) _FloginCheck__900201x__standardCheck() {
 			if true == __Vdecode.ok {
 				switch __Vdecode.Type {
 				case Cmd__loginS1ReqTryNoToken:
+					_FloginCheck_step102__sReply_token2(&__Vdecode, ___Vlc.ucCHSendLO)
 				default:
 					_FpfNdb(" 838392 07 : unknow how to deal with : %d", __Vdecode.Type)
 				}
---
+++ src0/base_1453__loginCheck_step01.go
@@ -0,0 +1,5 @@
+package main
+
+func _FloginCheck_step102__sReply_token2(___Vdecode *_Tdecode, ____VunCHsend *chan _TudpNodeDataSend) {
+	_FpfNdb(" 838393 01 : ...... ")
+}
