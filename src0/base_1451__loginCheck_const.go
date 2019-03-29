68adbe2ce70b4d7ad6c33626c5b3c0642fe956e2
Your Name <you@example.com>
2019-03-29 22:47:46 +0800

    _2019_0329__2247pm

--- src0/base_1451__loginCheck_const.go
+++ src0/base_1451__loginCheck_const.go
@@ -1,9 +1,15 @@
 package main
 
+// _TreqIneedToLogin
+type _TconnInfo struct {
+	ciId128 []byte
+}
+
 type _TloginCheck struct {
 	ucDecodeI         chan _Tdecode
 	ucCHSendLO        *chan _TudpNodeDataSend // ugCHSendI
 	ucCB900101init    func(*_TloginCheck)     // if nil , use the default init procedure
 	ucCB900201stCheck func(*_TloginCheck)     // if nil , use the default receive
-	ucMapStep101      map[[48]byte]_Tdecode
+	ucMapConnA        map[[16]byte]_TconnInfo // _TreqIneedToLogin MeIdx128
+	ucMapConnAcnt     int
 }
