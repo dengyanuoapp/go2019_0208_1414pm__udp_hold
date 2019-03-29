65a202dc99e1c91360ded914b9a7900464d3e8ba
Your Name <you@example.com>
2019-03-29 16:31:44 +0800

    _2019_0329__1631pm

--- src0/base_1459__loginCheck_irun.go
+++ src0/base_1459__loginCheck_irun.go
@@ -28,4 +28,20 @@ func (___Vlc *_TloginCheck) _FloginCheck__900101x__init__default() {
 }
 
 func (___Vlc *_TloginCheck) _FloginCheck__900201x__standardCheck() {
+	_Fsleep(_T1s)
+	if nil == ___Vlc.ucCHSendLO {
+		_FpfNdb(" 838392 01 why no Chan output ? exit loop")
+		return
+	}
+
+	_FpfN(" 838392 02 start")
+
+	var __Vdecode _Tdecode
+	for {
+		//_Fsleep_100s()
+		select {
+		case __Vdecode = <-___Vlc.ucDecodeI: // _Tdecode
+			_FpfNdb(" 838392 08 : %v", __Vdecode)
+		}
+	}
 }
