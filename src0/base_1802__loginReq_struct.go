9f69d0baf624dba248464fc910b14f751308d8f9
Your Name <you@example.com>
2019-04-01 01:19:14 +0800

    _2019_0401__0119am

--- src0/base_1802__loginReq_struct.go
+++ src0/base_1802__loginReq_struct.go
@@ -12,17 +12,26 @@ type _TloginReq struct {
 	TokenB   []byte
 }
 
+func _Fbyte2str(___Vb *[]byte) string {
+	if (nil == ___Vb) || (0 == len(*___Vb)) {
+		return ""
+	}
+	return _Pspf("%x", *___Vb)
+}
+
 func (___Vlr *_TloginReq) String() string {
 	__Vo := _Pspf(
-		" %d %s me:%x %x to:%x %x tokenAB: %x,%x ",
+		" %d %s %s me:%x %x to:%x %x tokenAB: %s,%s ",
 		___Vlr.MeTime,
 		___Vlr.ReqStr,
 		___Vlr.MeName,
+
 		___Vlr.MeIdx128,
 		___Vlr.MeSeq128,
 		___Vlr.ToIdx128,
 		___Vlr.ToSeq128,
-		___Vlr.TokenA,
-		___Vlr.TokenB)
+
+		_Fbyte2str(&___Vlr.TokenA),
+		_Fbyte2str(&___Vlr.TokenB))
 	return __Vo
 }
