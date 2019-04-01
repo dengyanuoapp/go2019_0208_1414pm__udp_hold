7058c189021b8610d3f86ad487386ae547fbafa8
Your Name <you@example.com>
2019-04-01 17:40:56 +0800

    _2019_0401__1740pm

--- src0/base_1422__decode_default_udp_rece.go
+++ src0/base_1422__decode_default_udp_rece.go
@@ -17,26 +17,30 @@ func _FudpDecode__700201x10__receive__default(___Vutm *_TuDecode) {
 		//_Fsleep_100s()
 		select {
 		case __Vundr = <-(*___Vutm.uTmCHunDataReceLI.unCHreceLO): // _TudpNodeDataRece
-			//_FpfNhex(&__Vundr.urrBuf, 38, " 388195 03 : %T , decode len <%d>: %11d", __Vundr.urrBuf, __Vundr.urrLen, _FtimeI64())
+			//_FpfNhex(&__Vundr.urrBuf, 38, " 388195 02 : %T , decode len <%d>: %11d", __Vundr.urrBuf, __Vundr.urrLen, _FtimeI64())
 			__Vundr._FdataPack__decode_from_udpNodeDataRece(&__Vdecode) // _Tdecode
 			__Vdecode.remoteAddr = __Vundr.urrRemoteAddr
 			__Vdecode.receiveTime = _FtimeInt()
 
 			switch __Vdecode.Type {
 			case Cmd__loginS1ReqTryNoToken, Cmd__loginS2ReplyTmpToken, Cmd__loginS3ReqWithToken: // 15540362231554036223
-				_FpfN(" 388195 05 : type %d, tokenA %x", __Vdecode.Type, __Vdecode.Dlogin.TokenA)
-				if nil != ___Vutm.uTmDecodeCmdLO {
-					_FpfN(" 388195 06 real outChain : type %d", __Vdecode.Type)
+				_FpfN(" 388195 03 : type %d, tokenA %x", __Vdecode.Type, __Vdecode.Dlogin.TokenA)
+				if nil == ___Vutm.uTmDecodeCmdLO {
+					_FpfN(" 388195 04 : outChan null , ignore ")
+				} else {
+					_FpfN(" 388195 05 real outChain : type %d", __Vdecode.Type)
 					(*___Vutm.uTmDecodeCmdLO) <- __Vdecode // 15540463611554046361
 				}
 			case Cmd__data:
-				_FpfN(" 388195 07 : type %d, tokenA %x", __Vdecode.Type, __Vdecode.Dlogin.TokenA)
-				if nil != ___Vutm.uTmDecodeDataLO {
+				_FpfN(" 388195 06 : type %d, tokenA %x", __Vdecode.Type, __Vdecode.Dlogin.TokenA)
+				if nil == ___Vutm.uTmDecodeDataLO {
+					_FpfN(" 388195 07 : outChan null , ignore ")
+				} else {
 					_FpfN(" 388195 08 real outChain : type %d", __Vdecode.Type)
 					(*___Vutm.uTmDecodeDataLO) <- __Vdecode //
 				}
 			default:
-				_FpfN(" 388195 08 : type %d", __Vdecode.Type)
+				_FpfN(" 388195 09 : type %d : unknow how to deal with.", __Vdecode.Type)
 			}
 		}
 	}
