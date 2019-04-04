1857b0384442b5e07484ca8edcfacb9410f2be60
Your Name <you@example.com>
2019-04-04 19:35:26 +0800

    _2019_0404__1935pm

--- src0/base_1451__loginCheck_const.go
+++ src0/base_1451__loginCheck_const.go
@@ -23,18 +23,34 @@ note :
 */
 
 // _TloginReq
-type _TconnInfo struct {
-	ciId128 []byte
-}
+//type _TconnInfo struct {
+//	ciId128 []byte
+//}
 
 type _TcmdMap struct {
 	M   map[[16]byte]_Tdecode // key by 16byte : MeIdx128
 	mux sync.Mutex
 }
-type _TdataMap struct {
-	M   map[[16]byte]_Tdecode // key by 16byte : MeIdx128
-	mux sync.Mutex
+
+func (___Vcm *_TcmdMap) String() string {
+	if 0 == len((*___Vcm).M) {
+		return ""
+	}
+
+	var __VstrO string
+
+	__Vi := 0
+	for ___Vk := range (*___Vcm).M {
+		__VstrO += _Pspf("%x ", ___Vk[:5])
+		__Vi++
+	}
+	return __VstrO
 }
+
+//type _TdataMap struct {
+//	M   map[[16]byte]_Tdecode // key by 16byte : MeIdx128
+//	mux sync.Mutex
+//}
 
 type _TloginCheck struct {
 	ulDecodeI           chan _Tdecode
@@ -44,9 +60,27 @@ type _TloginCheck struct {
 	ulCB900101init      func(*_TloginCheck) // if nil , use the default init procedure
 	ulCB900201stCheck   func(*_TloginCheck) // if nil , use the default receive
 	ulCmd               _TcmdMap
-	ulData              _TdataMap
-	ulTokenA            []byte
-	ulGenTime           int
+	//ulData              _TdataMap
+	ulTokenA  []byte
+	ulGenTime int
+}
+
+func String5(___Vb *[]byte) string {
+	if (nil == ___Vb) || (0 == len(*___Vb)) {
+		return ""
+	}
+	if len(*___Vb) < 5 {
+		return _Pspf("%x", *___Vb)
+	}
+	return _Pspf("%x", (*___Vb)[:5])
+}
+
+func (___Vlc *_TloginCheck) String() string {
+	return _Pspf(
+		"loginChck-cmdMap:{%s} key:%x,%d",
+		___Vlc.ulCmd.String(),
+		String5(&___Vlc.ulTokenA),
+		___Vlc.ulGenTime)
 }
 
 func _FcheckDecodeType(___Vdecode *_Tdecode, ___VwantType byte) bool { // match --> return false , others -> return true
--- src0/base_1459__loginCheck_irun.go
+++ src0/base_1459__loginCheck_irun.go
@@ -24,7 +24,7 @@ func (___Vlc *_TloginCheck) _FloginCheck__900101x__init__default() {
 	___Vlc.ulDecodeI = make(chan _Tdecode, 8)
 	___Vlc.ulCHconnPortI = make(chan _TudpConnPort, 8)
 	___Vlc.ulCmd.M = make(map[[16]byte]_Tdecode)
-	___Vlc.ulData.M = make(map[[16]byte]_Tdecode)
+	//___Vlc.ulData.M = make(map[[16]byte]_Tdecode)
 
 	_FpfNdb(" 838391 01 start ")
 	go _Frun(___Vlc, 900201) // _FloginCheck__900201x__standardCheck
