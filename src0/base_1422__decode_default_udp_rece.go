569ee2ec37f45844c9573cac1696aec3c376d47a
Your Name <you@example.com>
2019-04-05 08:41:31 +0800

    _2019_0405__0841am

--- src0/base_1422__decode_default_udp_rece.go
+++ src0/base_1422__decode_default_udp_rece.go
@@ -6,26 +6,27 @@ func _FudpDecode__700201x__receive__default(___Vutm *_TuDecode) {
 
 func _FudpDecode__700201x10__receive__default(___Vutm *_TuDecode) {
 	//_FpfNdb(" 388195 01 : filte received start ")
-	var __Vundr _TudpNodeDataRece
+	var __VunRece _TudpNodeDataRece
 	var __Vdecode _Tdecode
 	for {
 		//_Fsleep_100s()
 		select {
-		case __Vundr = <-___Vutm.uTmCHunDataReceI: // _TudpNodeDataRece
-			//_FpfNhex(&__Vundr.urrBuf, 38, " 388195 02 : %T , decode len <%d>: %11d", __Vundr.urrBuf, __Vundr.urrLen, _FtimeI64())
-			__Vundr._FdataPack__decode_from_udpNodeDataRece(&__Vdecode) // _Tdecode
-			__Vdecode.remoteAddr = __Vundr.urrRemoteAddr
+		case __VunRece = <-___Vutm.uTmCHunDataReceI: // _TudpNodeDataRece
+			//_FpfNhex(&__VunRece.urrBuf, 38, " 388195 02 : %T , decode len <%d>: %11d", __VunRece.urrBuf, __VunRece.urrLen, _FtimeI64())
+			__VunRece._FdataPack__decode_from_udpNodeDataRece(&__Vdecode) // _Tdecode
+			__Vdecode.remoteAddr = __VunRece.urrRemoteAddr
 			//__Vdecode.receiveTime = _FtimeInt()
+			_FpfNdb(" 388195 03 : decoder out:{%s} ", __Vdecode.String())
 
 			switch __Vdecode.Type {
 			case Cmd__loginS01genReplyTokenA, Cmd__loginS02genReplyTokenB,
 				Cmd__loginS03acceptWithToken: // , Cmd__loginS04acceptWithToken: // 15540362231554036223
-				//_FpfN(" 388195 03 : type %d, tokenA %x", __Vdecode.Type, __Vdecode.Dlogin.TokenL)
+				//_FpfN(" 388195 05 : type %d, tokenA %x", __Vdecode.Type, __Vdecode.Dlogin.TokenL)
 				if nil == ___Vutm.uTmDecodeCmdLO {
-					_FpfN(" 388195 04 : outChan null , ignore:%s", __Vdecode.String())
+					_FpfN(" 388195 06 : outChan null , ignore:%s", __Vdecode.String())
 				} else {
-					//_FpfN(" 388195 05 real outChain : type %d, tokenA %x", __Vdecode.Type, __Vdecode.Dlogin.TokenL)
-					//_FpfN(" 388195 06 real outChain : %s", __Vdecode.String())
+					//_FpfN(" 388195 08 real outChain : type %d, tokenA %x", __Vdecode.Type, __Vdecode.Dlogin.TokenL)
+					//_FpfN(" 388195 09 real outChain : %s", __Vdecode.String())
 					(*___Vutm.uTmDecodeCmdLO) <- __Vdecode // 15540463611554046361
 				}
 			case Cmd__data:
--- src0/base_1432__data_pack.go
+++ src0/base_1432__data_pack.go
@@ -15,7 +15,7 @@ func (___VuConnPort *_TudpConnPort) _FdataPack__101__udpConnPort(___VuTokenMe *[
 	if 0 != len(__Vreq.TokenR) {
 		_FpfNex(" 138185 01 : why len is not ZERO ?")
 	}
-	_FpfNex(" 138185 03 : gen loginReq , trying to package to bytes {%s}", __Vreq.String())
+	_FpfN(" 138185 03 : gen loginReq , trying to package to bytes {%s}", __Vreq.String())
 	__Vreq._FdataPack__100__loginReq(Cmd__loginS01genReplyTokenA, ___VoutBuf)
 	//return &__Vreq
 }
