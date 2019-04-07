6083195e3a70c89fa208640a9ca529a619ade713
Your Name <you@example.com>
2019-04-07 19:10:48 +0800

    _2019_0407__1910pm

--- src0/base_1242__rand_sleep.go
+++ src0/base_1242__rand_sleep.go
@@ -5,4 +5,11 @@ func _FsleepRand_12_to_14s() {
 	__Vi %= 3  // gen 0,1,2
 	__Vi += 12 // ok , 10 to 14s
 	_Fsleep_1sX(int(__Vi))
-} // _FsleepRand_10_to_14s
+} //
+
+func _FsleepRand_10_to_15s() {
+	__Vi := _FgenRand_uint32()
+	__Vi %= 6  // gen 0,1,2,3,4,5
+	__Vi += 10 // ok , 10 to 14s
+	_Fsleep_1sX(int(__Vi))
+} //
--- src0/base_1465__dataMachine_dataSendIdle.go
+++ src0/base_1465__dataMachine_dataSendIdle.go
@@ -1,4 +1,25 @@
 package main
 
 func _FdataMachin__1000502x__time_gap_dataSendIdle(___Vdm *_TdataMachine) {
+
+	for {
+		_FsleepRand_10_to_15s()
+		//dmmLastAccTime
+		__VkDelArr := [][16]byte{}
+		__Vnow2 := _FtimeInt()
+
+		___Vdm.dmMconn.mux.Lock()
+		for __Vk2, __Vv2 := range ___Vdm.dmMconn.M {
+			if __Vnow2-__Vv2.dmmLastAccTime > 57 {
+				__VkDelArr = append(__VkDelArr, __Vk2)
+			} else {
+				_FpfN(" 381921 01 : try send idle %x ", __Vk2)
+			}
+		}
+
+		for _, __Vk3 := range __VkDelArr {
+			_FpfN(" 381921 05 : try delete lost connect %x ", __Vk3)
+		}
+		___Vdm.dmMconn.mux.Unlock()
+	}
 }
