73d85e009d419c0d93b1b0f47d6b5012f190b7be
Your Name <you@example.com>
2019-04-07 20:22:36 +0800

    _2019_0407__2022pm

--- src0/base_1442__loginGenerator__chan_in.go
+++ src0/base_1442__loginGenerator__chan_in.go
@@ -5,12 +5,14 @@ func _FudpDecode__800201x__chan_in__default(___Vlg *_TloginGenerator) {
 		select {
 		case __Vdmid := <-___Vlg.lgCHdataMachineIdI: // _TdataMachinEid
 			//_FpfN(" 388192 01 ")
-			_FpfN(" 388192 02 : logGen received {%s}", __Vdmid.String())
-			__Vlen2 := len(__Vdmid.diIdx128)
+			//_FpfN(" 388192 02 : logGen received {%s}", __Vdmid.String())
+			__Vlen2 := len(__Vdmid.diSeq128)
 			switch __Vlen2 {
 			case 0:
+				_FpfN("388192 03 logGen lost {%s}", __Vdmid.String())
 				___Vlg.lgConnected = false
 			case 16:
+				_FpfN("388192 04 logGen ok {%s}", __Vdmid.String())
 				___Vlg.lgConnected = true
 			default:
 				_FpfN(" 388192 05 : why len %d ?", __Vlen2)
--- src0/base_1465__dataMachine_dataSendIdle.go
+++ src0/base_1465__dataMachine_dataSendIdle.go
@@ -18,7 +18,20 @@ func _FdataMachin__1000502x__time_gap_dataSendIdle(___Vdm *_TdataMachine) {
 		}
 
 		for _, __Vk3 := range __VkDelArr {
-			_FpfN(" 381921 05 : try delete lost connect %x ", __Vk3)
+			_FpfN(" 381921 05 : try delete lost connect %x in dataMachine", __Vk3)
+
+			if nil == ___Vdm.dmCHloginGenMachineIdLO {
+				_FpfNonce(" 381921 07 : try to stop lost connect %x in loginGen , but NULL out-chain", __Vk3)
+			} else {
+				_FpfNonce(" 381921 08 : try to stop lost connect %x in loginGen, ok ", __Vk3)
+				(*___Vdm.dmCHloginGenMachineIdLO) <- _TdataMachinEid{
+					diIdx128: ___Vdm.dmMconn.M[__Vk3].dmmID.diIdx128,
+				}
+			}
+
+			delete(___Vdm.dmMconn.M, __Vk3)
+			delete(___Vdm.dmMconn.LockLast, __Vk3)
+			delete(___Vdm.dmMconn.LockNow, __Vk3)
 		}
 		___Vdm.dmMconn.mux.Unlock()
 	}
