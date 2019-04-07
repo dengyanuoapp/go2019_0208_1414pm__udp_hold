d74b3d7e39e69828542f28284a74999cf244785d
Your Name <you@example.com>
2019-04-07 22:36:51 +0800

    _2019_0407__2236pm

--- src0/base_1465__dataMachine_dataSendIdle.go
+++ src0/base_1465__dataMachine_dataSendIdle.go
@@ -13,7 +13,11 @@ func _FdataMachin__1000502x__time_gap_dataSendIdle(___Vdm *_TdataMachine) {
 			if __Vnow2-__Vv2.dmmLastReadTime > 39 { // 3*(12+1) == 39
 				__VkDelArr = append(__VkDelArr, __Vk2)
 			} else {
-				_FpfN(" 381921 01 : %11d : try send idle %x ", _FtimeInt(), __Vk2)
+				if __Vnow2-__Vv2.dmmLastReadTime > 7 {
+					_FpfN(" 381921 01 : %11d : try send idle %x ", _FtimeInt(), __Vk2)
+				} else {
+					_FpfN(" 381921 02 : %11d : try send idle %x , but in 10s sent already. Skip. ", _FtimeInt(), __Vk2)
+				}
 			}
 		}
 
