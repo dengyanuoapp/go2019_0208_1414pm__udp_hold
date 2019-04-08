c18b7d5d2493beaafc26c4b325fb36f7b351ca3a
Your Name <you@example.com>
2019-04-08 13:16:49 +0800

    _2019_0408__1316pm

--- src0/base_1426__decode_default_gap_loop.go
+++ src0/base_1426__decode_default_gap_loop.go
@@ -3,12 +3,12 @@ package main
 func _FudpDecode__700301x__loop__default(___Vutm *_TuDecode) {
 	_FpfNdb(" 388194 01 : gap main loop start ")
 	if 0 == ___Vutm.uTmGapX {
-		_FpfN(" 388194 02 : Warn : why ZERO gap ? ")
+		_FpfNdb(" 388194 02 : Warn : why ZERO gap ? loop disabled")
 		return
 	}
 
 	for {
-		_FpfNdb(" 388194 02 : gap main loop ing ")
+		_FpfNdb(" 388194 03 : gap main loop ing ")
 		_Fsleep(___Vutm.uTmGapX)
 	}
 }
