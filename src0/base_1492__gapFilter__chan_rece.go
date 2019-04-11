e2d3ee6217132b9914b64ec0da7e0b7771518e8d
Your Name <you@example.com>
2019-04-11 13:51:49 +0800

    _2019_0411__1351pm

--- src0/base_1492__gapFilter__chan_rece.go
+++ src0/base_1492__gapFilter__chan_rece.go
@@ -1,1 +1,11 @@
 package main
+
+func (___Vgf *_TgapFilter) _FgapFilter__1200301x__Chan_rece() {
+	for {
+		select {
+		case <-___Vgf.gfCHdelay: // _TudpNodeDataReceX
+		case __Vur := <-___Vgf.gfCHudpNodeDataReceI: // _TudpNodeDataReceX
+		case __Vbyte := <-___Vgf.gfCHbyteI: // []byte
+		}
+	}
+}
--- src0/base_1499__gapFilter__irun.go
+++ src0/base_1499__gapFilter__irun.go
@@ -8,46 +8,49 @@ func (___Vgf *_TgapFilter) IRun(___Vidx int) {
 	switch ___Vidx {
 	case 1200101:
 		if nil == ___Vgf.gfCBinit {
-			go _FgapFilter__1200101x__init_default(___Vgf)
+			go ___Vgf.
+				_FgapFilter__1200101x__init_default()
 		} else {
-			go ___Vgf.gfCBinit(___Vgf)
+			go ___Vgf.
+				gfCBinit(___Vgf)
 		}
 	case 1200201:
 		if nil == ___Vgf.gfCBdelay {
-			go _FgapFilter__1200201x__gapLoop_delay_timeOut__generator(___Vgf)
+			go ___Vgf.
+				_FgapFilter__1200201x__gapLoop_delay_timeOut__generator()
 		} else {
-			go ___Vgf.gfCBdelay(___Vgf)
+			go ___Vgf.
+				gfCBdelay(___Vgf)
 		}
 	case 1200301:
 		if nil == ___Vgf.gfCBudpNodeDataRece {
-			go _FgapFilter__1200301x__udpNodeDataRece(___Vgf)
+			go ___Vgf.
+				_FgapFilter__1200301x__Chan_rece()
 		} else {
-			go ___Vgf.gfCBudpNodeDataRece(___Vgf)
-		}
-	case 1200302:
-		if nil == ___Vgf.gfCBbyte {
-			go _FgapFilter__1200302x__byte(___Vgf)
-		} else {
-			go ___Vgf.gfCBbyte(___Vgf)
+			go ___Vgf.
+				gfCBudpNodeDataRece(___Vgf)
 		}
 	default:
 		_FpfNex(" 839171 99 : unknown IRun : %d ", ___Vidx)
 	} // switch ___Vidx
 }
 
-func _FgapFilter__1200101x__init_default(___Vgf *_TgapFilter) {
+func (___Vgf *_TgapFilter) _FgapFilter__1200101x__init_default() {
 
 	if 0 == ___Vgf.gfGap {
 		_Fex(" 839171 01 : why Gap ZERO ? ")
 	}
+
+	___Vgf.gfCHudpNodeDataReceI = make(chan _TudpNodeDataRece, 50)
+	___Vgf.gfCHbyteI = make(chan []byte, 50)
+	___Vgf.gfCHdelay = make(chan byte, 50)
+
 	__VfCnt := 0
 
 	if nil != ___Vgf.gfCHudpNodeDataReceLO {
-		___Vgf.gfCHudpNodeDataReceI = make(chan _TudpNodeDataRece, 50)
 		__VfCnt++
 	}
 	if nil != ___Vgf.gfCHbyteLO {
-		___Vgf.gfCHbyteI = make(chan []byte, 50)
 		__VfCnt++
 	}
 
@@ -55,27 +58,19 @@ func _FgapFilter__1200101x__init_default(___Vgf *_TgapFilter) {
 		_Fex(" 839171 03 : why Chan Amount ZERO ? ")
 	}
 
-	___Vgf.gfCHdelay = make(chan byte, 50)
-
 	_Fsleep(_T100ms)
 
 	go _Frun(___Vgf, 1200201) // _FgapFilter__1200201x__gapLoop_delay_timeOut__generator
 
 	if nil != ___Vgf.gfCHudpNodeDataReceLO {
-		go _Frun(___Vgf, 1200301) // _FgapFilter__1200301x__udpNodeDataRece
-	}
-	if nil != ___Vgf.gfCHbyteLO {
-		go _Frun(___Vgf, 1200302) // _FgapFilter__1200302x__byte
+		go _Frun(___Vgf, 1200301) // _FgapFilter__1200301x__Chan_rece
 	}
 }
 
-func _FgapFilter__1200201x__gapLoop_delay_timeOut__generator(___Vgf *_TgapFilter) {
+func (___Vgf *_TgapFilter) _FgapFilter__1200201x__gapLoop_delay_timeOut__generator() {
 	for {
 		_Fsleep(___Vgf.gfGap)
 		___Vgf.gfCHdelay <- 1
+		_FpfN(" 839171 05 len(%d) cap(%d) ", len(___Vgf.gfCHdelay), cap(___Vgf.gfCHdelay))
 	}
-}
-func _FgapFilter__1200301x__udpNodeDataRece(___Vgf *_TgapFilter) {
-}
-func _FgapFilter__1200302x__byte(___Vgf *_TgapFilter) {
 }
