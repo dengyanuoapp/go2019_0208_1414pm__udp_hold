32a394215330ae1478d486b2ca1c49300bf97f7a
Your Name <you@example.com>
2019-04-15 15:01:00 +0800

    _2019_0415__1501pm

--- src0/base_1403__udpNode_default_rece.go
+++ src0/base_1403__udpNode_default_rece.go
@@ -72,22 +72,24 @@ func (___Vun *_TudpNodeSt) _FudpNode__500201y01__receive__default() {
 		UrrReceiveKey: ___Vun.unRkeyX,                    // _Tkey256
 	}
 
-	_CpfN(" 831818 07 Origin rece: me<%d> ra:<%s> (%d/%d){%x}<%x>",
+	_CpfN(" 831818 06 Origin rece: me<%d> ra:<%s> (%d/%d){%x}<%x>",
 		___Vun.unLocalPort, ___Vun.unRemoteAddr.String(),
 		___Vun.unRlen, len(__Vrece.UrrBuf), _FgenMd5__5(&__Vrece.UrrBuf), __Vrece.UrrBuf)
 
 	if nil != ___Vun.unCHreceStructLO {
 		(*___Vun.unCHreceStructLO) <- __Vrece
-		_FpfNonce(" 831818 08 Origin rece direct sent.")
+		_FpfNonce(" 831818 07 Origin rece direct sent.")
 
 	} else {
 		//(*___Vun.unCHreceByteLO) <- __Vrece
 		__VreceB, __Verr3 := _FencGob__only(&__Vrece)
 
 		if nil != __Verr3 {
-			_FpfN(" 831818 09 gob En error <%v>", __Verr3)
+			_FpfN(" 831818 08 gob En error <%v>", __Verr3)
 			return
 		}
+
+		_CpfN(" 831818 09 push to Byte-chan : <%x>", __VreceB)
 
 		(*___Vun.unCHreceByteLO) <- __VreceB
 	}
