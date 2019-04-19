f5ee86570075c3551454054fa46a62100ad9dc75
Your Name <you@example.com>
2019-04-19 19:42:25 +0800

    _2019_0419__1942pm

--- src0/base_1442__loginGenerator__chan_in.go
+++ src0/base_1442__loginGenerator__chan_in.go
@@ -15,12 +15,15 @@ func _Flogin__800201x__chan_in__default(___Vlg *_TloginGenerator) {
 			switch __Vlen2 {
 			case 0:
 				_FpfN("388192 03 logGen lost {%s} t:%11d ", __Vdmid.String(), _FtimeInt())
+				_CpfN("388192 04 logGen lost {%s} t:%11d ", __Vdmid.String(), _FtimeInt())
 				___Vlg.lgConnected = false
 			case 16:
-				_FpfNt("388192 04 logGen ok {%s} t:%11d ", __Vdmid.String(), _FtimeInt())
+				_FpfNt("388192 05 logGen ok {%s} t:%11d ", __Vdmid.String(), _FtimeInt())
+				_CpfN("388192 06 logGen ok {%s} t:%11d ", __Vdmid.String(), _FtimeInt())
 				___Vlg.lgConnected = true
 			default:
-				_FpfN(" 388192 05 : why len %d ? t:%11d ", __Vlen2)
+				_FpfN(" 388192 07 : why len %d ? t:%11d ", __Vlen2)
+				_CpfN(" 388192 08 : why len %d ? t:%11d ", __Vlen2)
 			}
 		}
 		___VudpDecode__800201x__mux.Unlock()
