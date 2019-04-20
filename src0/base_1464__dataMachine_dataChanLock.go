5cbf9b23b2b12e423f18bec5200dbd291b2f7cec
Your Name <you@example.com>
2019-04-21 00:47:43 +0800

    _2019_0421__0047am

--- src0/base_1464__dataMachine_dataChanLock.go
+++ src0/base_1464__dataMachine_dataChanLock.go
@@ -20,28 +20,36 @@ func (___Vdm *_TdataMachine) _FdataMachin__1000501y__swapLoginCkInfoForLock__swa
 		__Vc := &(___Vdm.dmMconn.dcsMm[__Vidx2]) // _TdataMachinEconnectClient
 		__Vcnt = __Vc.dccLockCntLast + __Vc.dccLockCntNow
 
+		__Vc.dccLockCntLast = __Vc.dccLockCntNow
 		if __Vcnt >= 3 { // one connect must have the amount of connect-portS large then 2
 			if nil == ___Vdm.dmCHloginGenMachineIdLO {
-				_FpfNonce(" 839196 01 connected succeed, but why loginGen NULL ?")
-				_CpfN(" 839196 02 connected succeed, but why loginGen NULL ?")
+				switch _VS.ProjName {
+				case "Fn":
+					// do nothing
+					_FpfNonce(" 839196 01 connected succeed, Fn do NOT have loginGen , so NULL.<%s>", _VS.ProjName)
+					_CpfN(" 839196 02 connected succeed, Fn do NOT have loginGen , so NULL.<%s>", _VS.ProjName)
+				default:
+					_FpfN(" 839196 03 connected succeed, but why loginGen NULL ? <%s>", _VS.ProjName)
+					_CpfN(" 839196 04 connected succeed, but why loginGen NULL ? <%s>", _VS.ProjName)
+				}
 			} else {
-				//_FpfN(" 839196 03 connected succeed, then told loginGen to stop ( push chan)")
-				_CpfN(" 839196 04 connected succeed, then told loginGen to stop ( push chan)")
+				//_FpfN(" 839196 05 connected succeed, then told loginGen to stop ( push chan)")
+				_CpfN(" 839196 06 connected succeed, then told loginGen to stop ( push chan)")
 				___Vout__dmCHloginGenMachineIdLO__mux.Lock()
 				(*___Vdm.dmCHloginGenMachineIdLO) <- ___Vdm.dmMconn.dcsMm[__Vidx2].dccID
 				___Vout__dmCHloginGenMachineIdLO__mux.Unlock()
+
+				__Vc.dccLockCntLast = 0 // clear the counter , forbit being next used
 			}
-			__Vc.dccLockCntLast = 0 // clear the counter , forbit being next used
-			__Vc.dccLockCntNow = 0  // clear the counter , forbit being next used
 
 			___Vdm.
 				_FdataMachin__1000501z__swapLoginCkInfoForLock__createNewDataClient(&__Vkey2, __Vidx2)
 		} else {
-			__Vc.dccLockCntLast = __Vc.dccLockCntNow
-			__Vc.dccLockCntNow = 0
-			//_FpfN(" 839196 07 no-enough, no swap. " )
-			_CpfN(" 839196 08 no-enough, no swap. ")
+			//_FpfN(" 839196 07 no-enough, swap only , no update . (%d)[%x]{%s}" , __Vidx2 , __Vkey2 , ___Vdm.dmMconn.String())
+			_CpfN(" 839196 08 no-enough, swap only , no update . (%d)[%x]{%s}", __Vidx2, __Vkey2, ___Vdm.dmMconn.String())
+
 		}
+		__Vc.dccLockCntNow = 0
 	}
 	_CpfN(" 839196 09 after swap {%s}", ___Vdm.dmMconn.String())
 }
@@ -51,10 +59,14 @@ func (___Vdm *_TdataMachine) _FdataMachin__1000501z__swapLoginCkInfoForLock__cre
 	// ___Vdm.dmMconn.dcsMm[__Vidx2].
 	_, __VdOk4 := ___Vdm.dmMdata.ddsMidx[*___Vkey] // _TdataMachinEdataSt _TdataMachinEdataClient
 	if __VdOk4 {
-		_FpfN(" 183818 01 : re-create client of {%s} ", ___Vdm.dmMconn.dcsMm[___VcIdx].dccID.String()) // _TdataMachinEdataSt _TdataMachinEdataClient
-		_FpfN(" 183818 02 : under-constructing")
-	} else {
-		_FpfN(" 183818 03 : create new client of {%s} ", ___Vdm.dmMconn.dcsMm[___VcIdx].dccID.String()) // _TdataMachinEdataSt _TdataMachinEdataClient
+		_CpfN(" 183818 01 : re-create client of {%s} ", ___Vdm.dmMconn.dcsMm[___VcIdx].dccID.String()) // _TdataMachinEdataSt _TdataMachinEdataClient
+		_CpfN(" 183818 02 : re-create client of {%s} ", ___Vdm.dmMdata.ddsMm[___VcIdx].ddcID.String()) // _TdataMachinEdataSt _TdataMachinEdataClient
+		_CpfN(" 183818 03 : under-constructing")
 		_FpfN(" 183818 04 : under-constructing")
+	} else {
+		_CpfN(" 183818 05 : create new client of {%s} ", ___Vdm.dmMconn.dcsMm[___VcIdx].dccID.String()) // _TdataMachinEdataSt _TdataMachinEdataClient
+		_CpfN(" 183818 06 : create new client of {%s} ", ___Vdm.dmMdata.ddsMm[___VcIdx].ddcID.String()) // _TdataMachinEdataSt _TdataMachinEdataClient
+		_CpfN(" 183818 07 : under-constructing")
+		_FpfN(" 183818 08 : under-constructing")
 	}
 }
