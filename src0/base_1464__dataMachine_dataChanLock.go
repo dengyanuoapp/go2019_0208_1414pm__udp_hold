e9ae0e13789a0af2eda71284cb5a4f00e17a64d9
Your Name <you@example.com>
2019-04-21 13:44:14 +0800

    _2019_0421__1344pm

--- src0/base_1464__dataMachine_dataChanLock.go
+++ src0/base_1464__dataMachine_dataChanLock.go
@@ -44,14 +44,14 @@ func (___Vdm *_TdataMachine) _FdataMachin__1000501y__swapLoginCkInfoForLock__swa
 			switch _VS.ProjName {
 			case "Fn":
 				// do nothing
-				_FpfNonce(" 839196 03 connected succeed, Fn do NOT have loginGen , so NULL.<%s>", _VS.ProjName)
+				_FpfN(" 839196 03 connected succeed, Fn do NOT have loginGen , so NULL.<%s>", _VS.ProjName)
 				_CpfN(" 839196 04 connected succeed, Fn do NOT have loginGen , so NULL.<%s>", _VS.ProjName)
 			default:
 				_FpfN(" 839196 05 connected succeed, but why loginGen NULL ? <%s>", _VS.ProjName)
 				_CpfN(" 839196 06 connected succeed, but why loginGen NULL ? <%s>", _VS.ProjName)
 			}
 		} else {
-			//_FpfN(" 839196 07 connected succeed, then told loginGen to stop ( push chan)")
+			_FpfN(" 839196 07 connected succeed, then told loginGen to stop ( push chan)")
 			_CpfN(" 839196 08 connected succeed, then told loginGen to stop ( push chan)")
 			___Vout__dmCHloginGenMachineIdLO__mux.Lock()
 			(*___Vdm.dmCHloginGenMachineIdLO) <- ___Vdm.dmMconn.dcsMm[__Vidx2].dccID
