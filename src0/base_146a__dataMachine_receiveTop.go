6325ba3ef12ee87bfc46bcad0e19e793bdab5622
Your Name <you@example.com>
2019-04-22 01:40:13 +0800

    _2019_0422__0140am

--- src0/base_146a__dataMachine_receiveTop.go
+++ src0/base_146a__dataMachine_receiveTop.go
@@ -59,16 +59,7 @@ func _FdataMachin__1000201x__receive__default(___Vdm *_TdataMachine) {
 	}
 }
 
-var ___VdmInfoLastTime int
-
 func (___Vdm *_TdataMachine) _FdataMachin__1000508__printDebugInfo() {
-	__Vt := _FtimeInt()
-	if __Vt-___VdmInfoLastTime < 3 {
-		return // ignore repeat print
-	}
-
-	___VdmInfoLastTime = __Vt
-
 	_NpfN(" 348181 01 : tcp rece ")
 	_CFpfN(" 348181 02 : _TdataMachine conn{%s} --- data{%s} \n",
 		___Vdm.dmMconn.String(), ___Vdm.dmMdata.String())
--- src0/base_1503__byteNoteBuf__chan_rece.go
+++ src0/base_1503__byteNoteBuf__chan_rece.go
@@ -1,11 +1,22 @@
 package main
 
+var ___VbyteNoteBuf__LastTime int
+
 func (___Vbnb *_TbyteNoteBuf) _FbyteNoteBuf__1300201x__chan_rece__default() {
 	for {
+
 		select {
+
 		case __Vb := <-___Vbnb.bnbCHinI:
 
-			_CFpfN(" 838191 01 _TbyteNoteBuf ")
+			__Vt := _FtimeInt()
+			if __Vt-___VbyteNoteBuf__LastTime < 3 {
+				continue // ignore repeat print
+			}
+
+			___VbyteNoteBuf__LastTime = __Vt
+
+			_NpfN(" 838191 01 _TbyteNoteBuf ")
 
 			if nil != ___Vbnb.bnbCHoutLO1 {
 				//_CFpfN(" 838191 11 _TbyteNoteBuf out : %d , %d", len(*___Vbnb.bnbCHoutLO1), cap(*___Vbnb.bnbCHoutLO1))
