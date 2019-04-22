04f470a7672f225e383e71f17ed3f323a23e5360
Your Name <you@example.com>
2019-04-22 22:45:40 +0800

    _2019_0422__2245pm

--- src0/base_1511__recePackThenEncodeAsLoad__const.go
+++ src0/base_1511__recePackThenEncodeAsLoad__const.go
@@ -7,7 +7,7 @@ import "time"
 type _TrecePackThenEncodeAsLoad struct {
 	pelGap                 time.Duration // _T1s
 	pelCHudpNodeDataReceBI chan []byte
-	pelCHencodeBLO         *chan _TdataTran
+	pelCHencodeBLO         *chan []byte
 	pelCBinit              func(*_TrecePackThenEncodeAsLoad)
 	pelChOutputGenGap      chan byte // every 1s gen output
 }
