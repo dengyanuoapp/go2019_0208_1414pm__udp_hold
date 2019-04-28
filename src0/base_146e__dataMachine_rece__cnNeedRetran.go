ea32e4d1e2f11374aa8b28915164cb0a1e49ba94
Your Name <you@example.com>
2019-04-29 01:08:39 +0800

    _2019_0429__0108am

--- src0/base_146e__dataMachine_rece__cnNeedRetran.go
+++ src0/base_146e__dataMachine_rece__cnNeedRetran.go
@@ -3,7 +3,7 @@ package main
 func (___Vdm *_TdataMachine) _FdataMachin__1000503x__cnNeedDnRetranOrDirectConnect(___Vc2sEncodeB *[]byte) {
 	// _FrecePackThenEncodeAsLoad__1400201y__decode_and_check_and_repack pelCHc2sEncodeBLO
 
-	_CFpfN(" 839199 01 : _TdataMachine : cnNeedDnRetranOrDirectConnect :<%s> ", String9(___Vc2sEncodeB))
+	__FpfN(" 839199 01 : _TdataMachine : cnNeedDnRetranOrDirectConnect :<%s> ", String9(___Vc2sEncodeB))
 
 	__Venc := _Tencode{} // _TencodeX
 	//_FdataPack__dePackUdpNodeRece__decode(&__Venc, *___Vc2sEncodeB)
@@ -17,6 +17,28 @@ func (___Vdm *_TdataMachine) _FdataMachin__1000503x__cnNeedDnRetranOrDirectConne
 	if __Venc.EnData.DDcmd != DDType__c2s { // byte
 		_CFpfN(" 839199 03 : _TdataMachine : DDcmd error :<%s> {%s}",
 			String9(___Vc2sEncodeB), __Venc.String())
+		return
+	}
+
+	__Vlen2 := len(___Vdm.dmMdata.ddsMidx) // _TdataMachinEdataSt
+	if 1 > __Vlen2 {
+		_CFpfN(" 839199 04 : _TdataMachine : c2s received , but , not Dn avaiable. ")
+		return
+	}
+
+	__VidxRand := _FgenRand_int() % __Vlen2
+
+	var __VdmClient *_TdataMachinEdataClient = nil
+
+	for _, __Vv3 := range ___Vdm.dmMdata.ddsMidx { // _TdataMachinEdataSt // ddsMidx map[[16]byte]int
+		if __Vv3 == __VidxRand {
+			__VdmClient = &(___Vdm.dmMdata.ddsMm[__Vv3]) // _TdataMachinEdataClient
+			break
+		}
+	}
+
+	if nil == __VdmClient {
+		_CFpfN(" 839199 05 : _TdataMachine : NULL ! what happens ? ")
 		return
 	}
 
