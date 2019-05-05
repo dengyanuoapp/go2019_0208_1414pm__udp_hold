e29ad9aa6881fba03440ab602ec8d62f96e9272d
Your Name <you@example.com>
2019-05-05 14:40:36 +0800

    _2019_0505__1440pm

--- src0/base_1524__tcpBufMachine__chain_rece_decode.go
+++ src0/base_1524__tcpBufMachine__chain_rece_decode.go
@@ -10,4 +10,11 @@ func (___Vtbm *_TtcpBufMachine) _FtcpBufMachine__1500201y1__chan_rece__Local2Rem
 	_FerrExit(" 381811 02 : ", __Verr2)
 
 	_CFpfN(" 381811 03 : tcpBuf rece from local {%s} ", __VtnRece.String()) // _TtcpNodeDataRece
+	if false == ___Vtbm._Finsert_local2remote_buf(&__VtnRece) {
+		return
+	}
+}
+
+func (___Vtbm *_TtcpBufMachine) _Finsert_local2remote_buf(___VtnRece *_TtcpNodeDataRece) bool {
+	return true
 }
