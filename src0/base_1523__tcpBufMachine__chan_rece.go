a3c68142a50c59b2a09fb4697a37bdead85a4eeb
Your Name <you@example.com>
2019-05-08 22:18:29 +0800

    _2019_0508__2218pm

--- src0/base_1523__tcpBufMachine__chan_rece.go
+++ src0/base_1523__tcpBufMachine__chan_rece.go
@@ -19,7 +19,7 @@ func (___Vtbm *_TtcpBufMachine) _FtcpBufMachine__1500201x__chan_rece__default() 
 				_FtcpBufMachine__1500201y1__chan_rece__Local2Remote(&__Vb)
 		case __VcmdB17 := <-___Vtbm.tbmCHtcpReceCmdI:
 			___VtcpBufMachine__1500201__mutex.Lock()
-			__FpfN(" 398381 03 : _TtcpBufMachine received Cmmd %d , {%x},", __VcmdB17[16], __VcmdB17[:3])
+			_CFpfN(" ###### 398381 03 : _TtcpBufMachine received Cmmd %d , {%x},", __VcmdB17[16], __VcmdB17[:3])
 
 		}
 
